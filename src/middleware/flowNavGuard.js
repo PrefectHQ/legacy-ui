26a4f83bd606780dee10974191a4f457feddd43e
nicholas <znicholasbrown@gmail.com>
2020-07-16 16:24:17 -0400

    Add middleware layer

---
+++ src/middleware/flowNavGuard.js
@@ -0,0 +1,41 @@
+import { defaultOptions } from '@/vue-apollo'
+import { createApolloClient } from 'vue-cli-plugin-apollo/graphql-client'
+
+const apolloClient = createApolloClient({ ...defaultOptions }).apolloClient
+
+const flowNavGuard = async (to, from, next) => {
+  let id = to.params?.id
+
+  let group = await apolloClient.query({
+    query: require('@/graphql/Middleware/flow-group.gql'),
+    variables: {
+      id: id
+    }
+  })
+
+  if (group?.data?.flow_group_by_pk?.id) return next()
+
+  let version = await apolloClient.query({
+    query: require('@/graphql/Middleware/flow.gql'),
+    variables: {
+      id: id
+    }
+  })
+
+  if (version?.data?.flow_by_pk?.flow_group_id) {
+    return next({
+      name: to.name,
+      params: { id: version.data.flow_by_pk.flow_group_id },
+      query: {
+        version: version.data.flow_by_pk.version,
+        ...to.query
+      }
+    })
+  }
+
+  return next({
+    name: 'not-found'
+  })
+}
+
+export default flowNavGuard
---
+++ src/middleware/tenantNavGuard.js
@@ -0,0 +1,69 @@
+import store from '@/store/index'
+
+const tenantNavGuard = async (to, from, next) => {
+  const passedTenantSlug = to.params.tenant
+
+  let membershipId
+  if (store.getters['tenant/tenantIsSet']) {
+    if (passedTenantSlug) {
+      if (passedTenantSlug == store.getters['tenant/tenant'].slug) {
+        return next()
+      } else {
+        let membership = store.getters['user/memberships'].find(
+          memb => memb.tenant.slug == passedTenantSlug
+        )
+        if (!membership)
+          return next({
+            name: 'not-found'
+          })
+        membershipId = membership.id
+      }
+    } else {
+      let membership = store.getters['user/memberships'].find(
+        memb => memb.tenant.id == store.getters['tenant/tenant'].id
+      )
+      if (!membership)
+        return next({
+          name: 'not-found'
+        })
+      membershipId = membership.id
+    }
+  } else {
+    if (passedTenantSlug) {
+      let membership = store.getters['user/memberships'].find(
+        memb => memb.tenant.slug == passedTenantSlug
+      )
+      if (!membership)
+        return next({
+          name: 'not-found'
+        })
+      membershipId = membership.id
+    } else {
+      membershipId = store.getters['user/defaultMembershipId']
+    }
+  }
+
+  if (membershipId) {
+    await store.dispatch('tenant/getTenant', membershipId)
+  } else {
+    await store.dispatch(
+      'tenant/getTenant',
+      store.getters['user/memberships'][0].id
+    )
+  }
+
+  await store.dispatch('license/getLicense')
+
+  /* eslint-disable require-atomic-updates */
+  to.params.tenant = store.getters['tenant/tenant'].slug
+  /* eslint-enable require-atomic-updates */
+
+  return next({
+    name: to.name,
+    replace: true,
+    params: to.params,
+    query: to.query
+  })
+}
+
+export default tenantNavGuard
