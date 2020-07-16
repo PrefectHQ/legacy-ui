52be1ba5e2d1a13200022a182abe96cab426e165
nicholas <znicholasbrown@gmail.com>
2020-07-16 15:55:43 -0400

    Add mixins

---
+++ src/mixins/changeStateMixin.js
@@ -0,0 +1,276 @@
+import { mapGetters } from 'vuex'
+
+export const changeStateMixin = {
+  props: {
+    taskRun: {
+      default: null,
+      type: Object
+    },
+    flowRun: {
+      default: null,
+      type: Object
+    },
+    dialogType: {
+      required: true,
+      type: String
+    }
+  },
+  data() {
+    return {
+      markAsDialog: false,
+      markAsLoading: false,
+      resumeLoad: '',
+      reason: '',
+      e1: 1,
+      formError: false,
+      name: 'PrefectUIRunMarkAsButton',
+      setStateError: false,
+      selectedState: '',
+      allTasks: false,
+      cancelLoad: false,
+      setStateSuccessA: false,
+      setStateSuccessB: false,
+      toastedMessage: 'We hit a problem.  Please try marking the state again.',
+      taskStates: [
+        'Finished',
+        'Success',
+        'Skipped',
+        'Failed',
+        'Looped',
+        'Mapped',
+        'Cached',
+        'TimedOut',
+        'Paused',
+        'Resume',
+        'TriggerFailed'
+      ],
+      flowStates: [
+        { header: 'Finished States' },
+        { divider: '...' },
+        'Failed',
+        'Success',
+        'Mapped',
+        'Finished',
+        'Cached',
+        'Skipped',
+        'TriggerFailed',
+        'TimedOut',
+        'Cancelled',
+        { header: 'Scheduled - To Re-run Flow Run' },
+        { divider: '...' },
+        'Scheduled',
+        { header: 'Pending States' },
+        { divider: '...' },
+        'Pending',
+        'Paused'
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters('user', ['user']),
+    ...mapGetters('tenant', ['role']),
+    filteredStates() {
+      if (this.dialogType === 'task run') {
+        return this.taskStates.filter(state => state !== this.taskRun.state)
+      } else {
+        return this.flowStates.filter(state => state !== this.flowRun.state)
+      }
+    },
+    checkVersion() {
+      if (
+        this.flowRun.flow.core_version[0] > 0 ||
+        this.flowRun.flow.core_version[2] > 7 ||
+        (this.flowRun.flow.core_version[2] > 6 &&
+          this.flowRun.flow.core_version[4] > 2)
+      ) {
+        return true
+      } else {
+        return false
+      }
+    }
+  },
+  methods: {
+    runLogMessage() {
+      if (this.dialogType == 'resume') {
+        return `Task ${this.taskRun.task.name} approved by ${this.user.username}`
+      } else if (this.reason) {
+        return `${this.user.username} marked ${this.dialogType} as ${this.selectedState} because "${this.reason}"`
+      } else {
+        return `${this.user.username} marked ${this.dialogType} 
+        as ${this.selectedState}`
+      }
+    },
+    activeButton() {
+      if (this.role === 'READ_ONLY_USER') return false
+      if (this.dialogType === 'task run') {
+        if (this.taskRun.state === 'Running') return true
+        return !!this.taskStates.includes(this.taskRun.state)
+      } else if (this.dialogType === 'flow run') {
+        return true
+      }
+    },
+    async writeLogs() {
+      const { data } = await this.$apollo.mutate({
+        mutation: require('@/graphql/Update/writelogs.gql'),
+        variables: {
+          flowRunId: this.taskRun ? this.taskRun.flow_run?.id : this.flowRun.id,
+          taskRunId: this.taskRun ? this.taskRun.id : null,
+          name: this.name,
+          message: this.runLogMessage()
+        }
+      })
+      return data && data.writeRunLogs && data.writeRunLogs.success
+    },
+    resumeRun() {
+      this.resumeLoad = this.taskRun.id
+      this.selectedState = 'Resume'
+      this.toastedMessage =
+        'An error occurred when approving this task run. Please try again.'
+      this.changeState()
+    },
+    cancelFlowRun() {
+      this.cancelLoad = true
+      this.selectedState = 'Cancelled'
+      this.allTasks = true
+      this.toastedMessage =
+        'Please try pressing cancel again. If the flow run has finished, use "Mark As" instead.'
+      this.changeState()
+    },
+    async changeState() {
+      try {
+        this.markAsLoading = true
+        const logSuccess = this.writeLogs()
+        if (logSuccess) {
+          if (
+            this.dialogType === 'task run' ||
+            this.dialogType == 'resume' ||
+            (this.allTasks && this.flowRun.task_runs.length)
+          ) {
+            let taskState
+            if (this.dialogType === 'task run' || this.dialogType == 'resume') {
+              taskState = {
+                taskRunId: this.taskRun.id,
+                version: this.taskRun.version,
+                state: {
+                  type: this.selectedState,
+                  message: this.runLogMessage()
+                }
+              }
+            } else {
+              taskState = this.flowRun.task_runs.map(taskRun => {
+                return {
+                  version: taskRun.version,
+                  taskRunId: taskRun.id,
+                  state: {
+                    type: this.selectedState,
+                    message: this.runLogMessage()
+                  }
+                }
+              })
+            }
+            const result = await this.$apollo.mutate({
+              mutation: require('@/graphql/TaskRun/set-task-run-states.gql'),
+              variables: {
+                setTaskRunStatesInput: taskState
+              }
+            })
+            this.setStateSuccessA =
+              result &&
+              result.data &&
+              result.data.setTaskRunStates &&
+              result.data.setTaskRunStates.states &&
+              result.data.setTaskRunStates.states.length
+            if (!this.setStateSuccessA) {
+              this.setStateError = true
+            }
+          }
+          if (this.dialogType === 'flow run') {
+            const result = await this.$apollo.mutate({
+              mutation: require('@/graphql/TaskRun/set-flow-run-states.gql'),
+              variables: {
+                flowRunId: this.flowRun.id,
+                version: this.flowRun.version,
+                state: {
+                  type: this.selectedState,
+                  message: this.runLogMessage()
+                }
+              }
+            })
+            this.setStateSuccessB =
+              result &&
+              result.data &&
+              result.data.setFlowRunStates &&
+              result.data.setFlowRunStates.states &&
+              result.data.setFlowRunStates.states.length
+            if (!this.setStateSuccessB) {
+              this.setStateError = true
+            }
+          }
+        } else {
+          this.setStateError = true
+        }
+      } catch (error) {
+        this.setStateError = true
+      }
+      if (this.setStateError) {
+        this.$toasted.show(this.toastedMessage, {
+          containerClass: 'toast-typography',
+          type: 'error',
+          icon: 'error',
+          action: {
+            text: 'Close',
+            onClick(e, toastObject) {
+              toastObject.goAway(0)
+            }
+          },
+          duration: 3000
+        })
+      }
+      if (
+        this.setStateSuccessA &&
+        this.setStateSuccessB &&
+        this.cancelLoad &&
+        !this.setStateError
+      ) {
+        this.$toasted.show(
+          'Flow run set for cancel.  This may take 30 seconds.',
+          {
+            containerClass: 'toast-typography',
+            type: 'success',
+            icon: 'check_circle',
+            action: {
+              text: 'Close',
+              onClick(e, toastObject) {
+                toastObject.goAway(0)
+              }
+            },
+            duration: 3000
+          }
+        )
+      }
+      setTimeout(() => this.reset(), 500)
+    },
+    checkContinue() {
+      if (this.selectedState) {
+        this.e1 = 2
+      }
+    },
+    reset() {
+      this.markAsDialog = false
+      this.markAsLoading = false
+      this.reason = ''
+      this.form = false
+      this.setStateError = false
+      this.e1 = 1
+      this.selectedState = ''
+      this.allTasks = false
+      this.cancelLoad = false
+      this.toastedMessage =
+        'We hit a problem.  Please try marking the state again.'
+      this.setStateSuccessA = false
+      this.setStateSuccessB = false
+      this.cancelLoad = false
+      this.resumeLoad = ''
+    }
+  }
+}
---
+++ src/mixins/teamProfileMixin.js
@@ -0,0 +1,326 @@
+import { mapGetters, mapActions } from 'vuex'
+
+export const teamProfileMixin = {
+  data() {
+    return {
+      // User inputs
+      tenantChanges: {
+        name: '',
+        slug: ''
+      },
+
+      // Regexes to check if a slug is invalid
+      slugCharRegex: /^[a-z0-9|-]*$/,
+      dashPositionRegex: /^(-)|-{2,}|(-)$/,
+
+      // Flags that are set to true during async vaidation checks
+      isCheckingName: false,
+      isCheckingSlug: false,
+
+      // Flag input icons so that they are only revealed during submission & validation
+      showNameIcon: false,
+      showSlugIcon: false,
+
+      // Input errors
+      nameErrors: [],
+      slugErrors: [],
+
+      // Form is submitted & tenant is updating
+      isUpdatingTenant: false,
+
+      // Tenant update returns a server error
+      updateServerError: false
+    }
+  },
+  computed: {
+    ...mapGetters('tenant', ['tenant']),
+    isTenantAdmin() {
+      return this.tenant.role === 'TENANT_ADMIN'
+    },
+    isUpdatable() {
+      // Tenant cannot be updated during any kind of load state
+      if (this.isUpdatingTenant) return false
+      if (this.isCheckingSlug) return false
+
+      // Tenant cannot be updated if any errors exist
+      if (
+        this.updateServerError ||
+        this.nameErrors.length > 0 ||
+        this.slugErrors.length > 0
+      ) {
+        return false
+      }
+
+      return (
+        this.tenantChanges.name !== this.tenant.name ||
+        this.tenantChanges.slug !== this.tenant.slug
+      )
+    },
+    slug: {
+      get() {
+        return this.tenantChanges.slug
+      },
+      set(val) {
+        this.tenantChanges.slug = val.replace(' ', '-')
+      }
+    },
+    name: {
+      get() {
+        return this.tenantChanges.name
+      },
+      set(val) {
+        this.tenantChanges.name = val
+      }
+    },
+    showNameCheck() {
+      return (
+        this.showNameIcon &&
+        this.nameErrors.length === 0 &&
+        !this.updateServerError
+      )
+    },
+    showNameClear() {
+      return (
+        this.showNameIcon &&
+        (this.nameErrors.length > 0 || this.updateServerError)
+      )
+    },
+    showSlugCheck() {
+      return (
+        this.showSlugIcon &&
+        this.slugErrors.length === 0 &&
+        !this.updateServerError
+      )
+    },
+    showSlugClear() {
+      return (
+        this.showSlugIcon &&
+        (this.slugErrors.length > 0 || this.updateServerError)
+      )
+    }
+  },
+  watch: {
+    tenant(val) {
+      this.tenantChanges.name = val.name || ''
+      this.tenantChanges.slug = val.slug || ''
+    }
+  },
+  created() {
+    this.tenantChanges.name = this.tenant.name || ''
+    this.tenantChanges.slug = this.tenant.slug || ''
+  },
+  methods: {
+    ...mapActions('tenant', ['getTenant', 'updateTenantSettings']),
+    ...mapActions('alert', ['setAlert']),
+    // Synchronous name check that occur on input blur
+    checkName(name) {
+      this.nameErrors = []
+
+      // If name is not provided, reset to original value
+      //
+      // Make this field required if there is no original value
+      if (name === '') {
+        if (!this.tenant.name) {
+          this.nameErrors = ['A team name is required.']
+        } else {
+          this.tenantChanges.name = this.tenant.name
+        }
+        return
+      }
+
+      if (name.length < 3) {
+        this.nameErrors = ['Team names must contain a minimum of 3 characters.']
+        return
+      }
+    },
+    // Synchronous slug input check that occurs on input blur
+    checkSlug(slug) {
+      this.slugErrors = []
+
+      // If slug is not provided, reset to original value
+      //
+      // Make this field required if there is no original value
+      if (slug === '') {
+        if (!this.tenant.slug) {
+          this.slugErrors = ['A URL slug for your team is required.']
+        } else {
+          this.tenantChanges.slug = this.tenant.slug
+        }
+        return
+      }
+
+      if (slug.length < 3) {
+        this.slugErrors = ['URL slug must contain a minimum of 3 characters.']
+        return
+      }
+
+      if (!this.slugCharRegex.test(slug)) {
+        this.slugErrors = [
+          'URL slugs can only contain lowercase letters, numbers, and dashes.'
+        ]
+        return
+      }
+
+      if (this.dashPositionRegex.test(slug)) {
+        this.slugErrors = [
+          'URL slugs cannot begin, end, or contain more than 1 dash in a row.'
+        ]
+      }
+    },
+    // Asynchronous name input "check" that occurs when user attempts to submit the form
+    //
+    // Quotes around "check" because a check doesn't actually occur
+    //
+    // Instead, a timeout is returned purely to surface a loader on the UI to keep it
+    // inline with the slug async check
+    checkNameAsync() {
+      // Stop immediately if the input never changed
+      if (this.showNameIcon) return
+
+      this.isCheckingName = true
+
+      return new Promise(resolve => {
+        setTimeout(() => {
+          this.showNameIcon = true
+          this.isCheckingName = false
+          resolve()
+        }, 750)
+      })
+    },
+    // Asynchronous slug input check that occurs when user attempts to submit the form
+    //
+    // Currently checks only for slug uniqueness
+    checkSlugAsync() {
+      // Stop immediately if the input never changed
+      if (this.showSlugIcon) return
+
+      this.isCheckingSlug = true
+
+      return new Promise(resolve => [
+        setTimeout(async () => {
+          // Check slug uniqueness
+          try {
+            const checkSlug = await this.$apollo.query({
+              query: require('@/graphql/Tenant/tenant-by-slug.gql'),
+              variables: {
+                slug: this.slug
+              }
+            })
+
+            if (
+              checkSlug.data &&
+              checkSlug.data.tenant.length !== 0 &&
+              this.slug != this.tenant.slug
+            ) {
+              this.slugErrors = ['Sorry, that URL slug is already in use.']
+            }
+          } catch {
+            this.updateServerError = true
+          }
+
+          this.showSlugIcon = true
+          this.isCheckingSlug = false
+          resolve()
+        }, 750)
+      ])
+    },
+    resetNameMetadata() {
+      this.showNameIcon = false
+      this.nameErrors = []
+      this.updateServerError = false
+    },
+    resetSlugMetadata() {
+      this.showSlugIcon = false
+      this.slugErrors = []
+      this.updateServerError = false
+    },
+    handleError() {
+      this.setAlert({
+        alertMessage:
+          'Something went wrong while trying to update your team profile settings. Please try again later.',
+        alertType: 'error',
+        alertShow: true
+      })
+    },
+    handleSuccess() {
+      this.setAlert({
+        alertMessage:
+          "Your team's profile settings have been successfully updated.",
+        alertType: 'success',
+        alertShow: true
+      })
+      setTimeout(() => {
+        this.resetNameMetadata()
+        this.resetSlugMetadata()
+      }, 3000)
+    },
+    async updateTenant() {
+      if (this.nameErrors.length > 0 || this.slugErrors.length > 0) {
+        return
+      }
+      this.updateServerError = false
+      this.isUpdatingTenant = true
+
+      // Async tenant checks
+      await this.checkNameAsync()
+      await this.checkSlugAsync()
+
+      if (
+        this.tenantChanges.slug == this.tenant.slug &&
+        this.tenantChanges.name == this.tenant.name
+      ) {
+        await this.updateTenantSettings({
+          teamNamed: true
+        })
+
+        this.handleSuccess()
+        this.isUpdatingTenant = false
+        return
+      }
+
+      let newTenant
+
+      try {
+        newTenant = await this.$apollo.mutate({
+          mutation: require('@/graphql/Tenant/update-tenant.gql'),
+          variables: {
+            name: this.tenantChanges.name || this.tenant.name,
+            slug: this.tenantChanges.slug || this.tenant.slug
+          }
+        })
+      } catch (e) {
+        // If this query failed because the slug name is already in use,
+        // then an inline error message will be surfaced.
+        // Otherwise, surface the global error alert.
+        if (e.message !== 'GraphQL error: Uniqueness violation.') {
+          this.handleError()
+          this.updateServerError = true
+        }
+        this.isUpdatingTenant = false
+        return
+      }
+
+      if (
+        !newTenant.data ||
+        !newTenant.data.updateTenantName ||
+        !newTenant.data.updateTenantSlug
+      ) {
+        this.handleError()
+        this.updateServerError = true
+        return
+      }
+
+      await this.getTenant(
+        newTenant.data.updateTenantSlug.tenant.memberships[0].id
+      )
+
+      this.$router.replace({
+        name: 'account',
+        params: { tenant: newTenant.data.updateTenantSlug.tenant.slug }
+      })
+
+      this.handleSuccess()
+      this.isUpdatingTenant = false
+    }
+  }
+}
