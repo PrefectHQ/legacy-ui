e3950f2014ccb9dffb21e3a2dcf39cd03fd7cf76
nicholas <znicholasbrown@gmail.com>
2020-07-21 15:30:07 -0400

    Add useful links to start page

--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,11 +1,40 @@
 <script>
 import ExternalLink from '@/components/ExternalLink'
 
+const exploreBlocks = [
+  {
+    headline: 'Read Case Studies',
+    body: 'Learn how our customers build on Prefect',
+    src: require('@/assets/icon-illustrations/tangible-investment.svg'),
+    alt: 'Case studies image',
+    href: 'https://www.prefect.io/why-prefect/case-studies',
+    linkText: 'Read Case Studies'
+  },
+  {
+    headline: 'Explore the Docs',
+    body: 'Get API references, walkthroughs, and tutorials',
+    src: require('@/assets/icon-illustrations/read-the-blog.svg'),
+    alt: 'Explore the docs image',
+    href: 'https://docs.prefect.io',
+    linkText: 'Get the Docs'
+  },
+  {
+    headline: 'Join our Community',
+    body: 'Chat with us, ask questions, and share tips',
+    src: require('@/assets/icon-illustrations/slack-community.svg'),
+    alt: 'Join the community image',
+    href:
+      'https://join.slack.com/t/prefect-community/shared_invite/enQtODQ3MTA2MjI4OTgyLTliYjEyYzljNTc2OThlMDE4YmViYzk3NDU4Y2EzMWZiODM0NmU3NjM0NjIyNWY0MGIxOGQzODMxNDMxYWYyOTE',
+    linkText: 'Join Slack'
+  }
+]
+
 export default {
   components: { ExternalLink },
   data() {
     return {
-      sdkTab: 'pip'
+      sdkTab: 'pip',
+      exploreBlocks: exploreBlocks
     }
   }
 }
@@ -36,7 +65,44 @@ export default {
           <v-card-title>
             Explore the possibilities with Prefect
           </v-card-title>
-          <v-card-text> </v-card-text>
+          <v-card-text>
+            <v-row>
+              <v-col
+                v-for="(block, i) in exploreBlocks"
+                :key="i"
+                class="d-flex flex-column text-center"
+                cols="12"
+                md="4"
+              >
+                <div>
+                  <img
+                    class="explore-image"
+                    :src="block.src"
+                    :alt="block.alt"
+                  />
+                </div>
+
+                <div class="text-h6">
+                  {{ block.headline }}
+                </div>
+                <div class="text-body-2 mb-4">
+                  {{ block.body }}
+                </div>
+                <div>
+                  <v-btn
+                    color="accentOrange"
+                    dark
+                    depressed
+                    small
+                    target="_blank"
+                    :href="block.href"
+                  >
+                    {{ block.linkText }}
+                  </v-btn>
+                </div>
+              </v-col>
+            </v-row>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -137,5 +203,10 @@ export default {
 .code-block {
   font-family: 'Source Code Pro', monospace !important;
   white-space: pre-wrap;
+}
+
+.explore-image {
+  height: 125px;
+  max-width: 125px;
 }
 </style>
