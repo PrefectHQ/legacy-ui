4fa8853573cd310c5a1b82896f2332785b2727d3
Nicholas Brown <nicholas@prefect.io>
2020-08-10 22:02:46 -0400

    Update src/mixins/changeStateMixin.js

--- src/mixins/changeStateMixin.js
+++ src/mixins/changeStateMixin.js
@@ -247,7 +247,7 @@ export const changeStateMixin = {
         this.setAlert({
           alertShow: true,
           alertMessage:
-            'Your flow run has been cancelled; please allow up to 30 seconds for this to take effect.',
+            'Your flow run will be cancelled; please allow some time for this to take effect.',
           alertType: 'success'
         })
       }
