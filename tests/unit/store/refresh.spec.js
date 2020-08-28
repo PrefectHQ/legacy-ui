62b13af5b1ed5dd72449b2cc83c1cb5a3d1a8881
Jenny <jenny@prefect.io>
2020-08-28 11:50:29 -0400

    update store tests

--- tests/unit/store/refresh.spec.js
+++ tests/unit/store/refresh.spec.js
@@ -1,13 +1,47 @@
-import store from '@/store'
+import refresh from '@/store/refresh'
+import { createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('Refresh Vuex Module', () => {
-  it('has a key that starts at 0', () => {
-    expect(store.getters['refresh/componentKey']).toBe(0)
+  const initialState = () => {
+    return {
+      componentKey: 0
+    }
+  }
+
+  describe('State', () => {
+    it('should be initally be set to 0', () => {
+      const state = refresh.state
+      expect(state.componentKey).toBe(0)
+    })
   })
 
-  it('has a key that increases by 1 when add is called', () => {
-    expect(store.getters['refresh/componentKey']).toBe(0)
-    store.commit('refresh/add')
-    expect(store.getters['refresh/componentKey']).toBe(1)
+  describe('getters', () => {
+    let store
+    store = new Vuex.Store({
+      state: initialState(),
+      getters: refresh.getters,
+      mutations: refresh.mutations
+    })
+    it('componentKey returns 0 in intial state', () => {
+      expect(store.getters.componentKey).toBe(0)
+    })
+  })
+
+  describe('mutations', () => {
+    let store
+    store = new Vuex.Store({
+      state: initialState(),
+      getters: refresh.getters,
+      mutations: refresh.mutations
+    })
+    it('add increase the componentKey by 1', () => {
+      expect(store.getters.componentKey).toBe(0)
+      store.commit('add')
+      expect(store.getters.componentKey).toBe(1)
+    })
   })
 })
