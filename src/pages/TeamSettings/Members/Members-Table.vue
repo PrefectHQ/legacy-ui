2a2f747fc48967eef54d6e92c50d88cebf3c29de
Jenny <jenny@prefect.io>
2020-09-03 12:00:00 -0400

    fix linter

--- src/pages/TeamSettings/Members/Members-Table.vue
+++ src/pages/TeamSettings/Members/Members-Table.vue
@@ -360,7 +360,9 @@ export default {
       @confirm="removeUser(selectedUser.membershipId)"
     >
       <div v-if="deleteSelfWarning" class="red--text"
-        >Are you sure you want to remove yourself from {{ tenant.name }}? You'll no longer be able to access your Projects or Flows associated with {{ tenant.name }}.
+        >Are you sure you want to remove yourself from {{ tenant.name }}? You'll
+        no longer be able to access your Projects or Flows associated with
+        {{ tenant.name }}.
       </div>
       <div v-else>
         <span class="font-weight-bold">{{ selectedUser.username }}</span>
