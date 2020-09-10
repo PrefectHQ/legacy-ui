9b2fb7a68d07cfc082b95b55a872f0e6146e0666
Nicholas Brown <nicholas@prefect.io>
2020-09-10 16:34:28 -0400

    Performance work part 2 (#212)
    * Remove refetch methods
    
    * Create workers directory, move schematic worker there
    
    * Rename schematic worker
    
    * Use document visibility API to prevent timeline chart updates
    
    * Remove running flows interval
    
    * Add some performance metrics
    
    * Update inMemoryCache => cache (so it's not confused with imported InMemoryCache)
    
    * Don't use moment for upcoming runs tile on the dashboard
    
    * Don't use moment on the upcoming runs tile on the flow page
    
    * Update update method on upcoming tile flow page
    
    * Update changelog
    
    * Remove event listeners when click area rects are exited
    
    * Check that late runs exist
    
    * Check upcoming and late runs in template
    
    * Enable performance config for dev environemnets and remove $ref in barchart computed prop
    
    * Add a component destroy mixin
    
    * Move barchart calcy and calcheight to component methods, remove local refs
    
    * Remove _-prefixed methods and props, remove listeners in destroy mixin
    
    * More work on the destroy method
    
    * Unset named event listener
    
    * Remove destroy method
    
    * Remove null event listener, update stacked line chart
    
    * Add some comments in main.js
    
    * Remove all underscore methods
    
    * Update changelog
    
    * Also null apollo
    
    * Update the SchematicFlow component to be more efficient during drawing and teardown
    
    * Remove transition on fitViz (makes this operation more consistent)

--- src/components/Schematics/Schematic-Flow.vue
+++ src/components/Schematics/Schematic-Flow.vue
@@ -3,6 +3,7 @@ import { mapGetters } from 'vuex'
 import * as d3_base from 'd3'
 import { zoom } from 'd3-zoom'
 import uniqueId from 'lodash.uniqueid'
+import throttle from 'lodash.throttle'
 import Legend from '@/components/Schematics/Legend'
 import LogRocket from 'logrocket'
 import SchematicNode from '@/components/Schematics/Schematic-Node'
@@ -88,6 +89,7 @@ export default {
       showLabels: false,
       showTooltip: true,
       size: 0,
+      timer: null,
       tooltipData: null,
       transitionDuration: 250,
       transform: null,
@@ -135,6 +137,9 @@ export default {
         level2: this.transform?.k / this.size > 0.15
       }
     },
+    animateCanvas: function() {
+      return throttle(this.rawAnimateCanvas, 16)
+    },
     visibleNodes() {
       if (!this.transform) return []
       const buf = 100 // buffer
@@ -160,13 +165,7 @@ export default {
       }
     },
     showCards() {
-      let t = d3.timer(elapsed => {
-        this.drawCanvas(this.transform)
-
-        if (elapsed > this.transitionDuration) {
-          t.stop()
-        }
-      })
+      this.animateCanvas()
     }
   },
   mounted() {
@@ -179,10 +178,14 @@ export default {
     }
   },
   beforeDestroy() {
-    this.id = uniqueId('schematic')
+    cancelAnimationFrame(this.drawCanvas)
+    this.id = null
     this.hasFit = null
     this.nodeData = null
     this.worker = null
+    this.custom = null
+    this.timer.stop()
+    this.timer = null
   },
   methods: {
     _zoomed(event) {
@@ -201,7 +204,7 @@ export default {
       this.transformEventK = event.transform.k
       this.transformEventX = event.transform.x
       this.transformEventY = event.transform.y
-      this.drawCanvas(this.transform)
+      requestAnimationFrame(this.drawCanvas)
     },
     _zoomIn() {
       if (this.transform.k < 0.01) return
@@ -262,7 +265,7 @@ export default {
       this.loadingKey--
     },
     async createDag(tasks) {
-      if (tasks.length === 0) return
+      if (tasks.length === 0 || this.loading) return
       this.loadingKey += 2
       this.mappedTasks = {}
 
@@ -409,19 +412,52 @@ export default {
       this.redraw()
     },
     fitViz() {
-      this.canvas
-        .transition()
-        .duration(500)
-        .call(
+      this.canvas.call(
+        this.scaledZoom.transform,
+        d3.zoomIdentity
+          .translate(this.canvasAdjustment.w / 2, this.canvasAdjustment.h / 2)
+          .scale(1)
+      )
+    },
+    rawAnimateCanvas() {
+      cancelAnimationFrame(this.drawCanvas)
+
+      this.timer?.stop()
+      this.timer = d3.timer(elapsed => {
+        this.canvas.call(
           this.scaledZoom.transform,
           d3.zoomIdentity
-            .translate(this.canvasAdjustment.w / 2, this.canvasAdjustment.h / 2)
-            .scale(1)
+            .translate(this.transformEventX, this.transformEventY)
+            .scale(this.transformEventK)
         )
+
+        if (elapsed > this.transitionDuration + 100) {
+          setTimeout(() => {
+            // Final fit
+            this.painting = false
+
+            // Fit after the first drawing
+            if (!this.hasFit) {
+              this.hasFit = true
+              this.fitViz()
+              setTimeout(() => {
+                this.showCanvas = true
+              }, 600)
+            }
+          }, 500)
+
+          this.timer.stop()
+        }
+      })
     },
-    drawCanvas(transform) {
+    drawCanvas() {
+      // If the custom ref has been destroyed we don't want to continue trying
+      // to draw the canvas (this can happen if this has been queued)
+      if (!this.custom) return
+
       this.context.clearRect(0, 0, this.width, this.height)
 
+      const transform = this.transform
       const context = this.context
       const showNodes = !this.showCards
       const showDetails = this.showDetails
@@ -437,6 +473,8 @@ export default {
         scaledArrow > 10 ? 10 : scaledArrow < 5 ? 5 : scaledArrow
       const nodeSize =
         scaledNode + Math.min(scaledNode, scaledNode * (size / scaledNode))
+
+      if (!this.custom) return
 
       let edges = this.custom.selectAll('path')
 
@@ -495,9 +533,11 @@ export default {
         })
       })
 
-      if (!showNodes && showDetails.level1) return
+      if (!showNodes && showDetails?.level1) return
 
       let nodes = this.custom.selectAll('circle')
+
+      if (!nodes) return
 
       // Resets the alpha before we draw nodes,
       // which we always want to be opaque
@@ -545,35 +585,7 @@ export default {
     redraw() {
       this.updateEdges()
       this.updateNodes()
-
-      let t = d3.timer(elapsed => {
-        this.drawCanvas(d3.zoomIdentity)
-
-        this.canvas.call(
-          this.scaledZoom.transform,
-          d3.zoomIdentity
-            .translate(this.transformEventX, this.transformEventY)
-            .scale(this.transformEventK)
-        )
-
-        if (elapsed > this.transitionDuration + 100) {
-          setTimeout(() => {
-            // Final fit
-            this.painting = false
-
-            // Fit after the first drawing
-            if (!this.hasFit) {
-              this.hasFit = true
-              this.fitViz()
-              setTimeout(() => {
-                this.showCanvas = true
-              }, 600)
-            }
-          }, 500)
-
-          t.stop()
-        }
-      })
+      this.animateCanvas()
     },
     updateNodes() {
       // Preserves references to this for event handlers
@@ -778,13 +790,8 @@ export default {
           query: { ...this.$route.query, schematic: '' }
         })
       }
-      let t = d3.timer(elapsed => {
-        this.drawCanvas(this.transform)
 
-        if (elapsed > this.transitionDuration + 100) {
-          t.stop()
-        }
-      })
+      this.animateCanvas()
     },
     showAllNodes() {
       this.custom
@@ -1025,7 +1032,7 @@ export default {
     >
       <v-tooltip top>
         <template v-slot:activator="{ on }">
-          <v-btn icon tile v-on="on" @click="fitViz()">
+          <v-btn icon tile v-on="on" @click="fitViz">
             <v-icon>center_focus_strong</v-icon>
           </v-btn>
         </template>
--- src/components/Visualizations/BarChart.vue
+++ src/components/Visualizations/BarChart.vue
@@ -26,10 +26,9 @@ export default {
       id: uniqueId('barChart'),
       animationTimer: null,
       bars: [],
-      barStart: null,
-      calcHeight: null,
-      calcY: null,
+      boundingClientRect: null,
       canvas: null,
+      chartHeight: null,
       chart: null,
       easing: 'easeCubic',
       groupBreaklines: null,
@@ -56,18 +55,35 @@ export default {
   },
   computed: {
     ...mapGetters('user', ['timezone']),
-    _items() {
+    computedItems() {
       return this.loading ? this.loadingItems : this.items
     },
-    _yField() {
+    computedMouseout: function() {
+      return debounce(this.rawMouseout, 0, { trailing: true, leading: false })
+    },
+    computedYField() {
       return this.loading ? 'value' : this.yField
     },
     animationDuration() {
       return this.loading ? 500 : 500
     },
+    barStart() {
+      return this.chartHeight - this.chartHeight * 0.1
+    },
+    animateCanvas: function() {
+      return throttle(this.rawAnimateCanvas, 16)
+    },
+    resizeChart: function() {
+      return debounce(() => {
+        requestAnimationFrame(this.rawResizeChart)
+      }, 150)
+    },
+    min() {
+      return this.chartHeight * 0.15
+    },
     tooltipStyle() {
       if (!this.hovered) return
-      let p = this.$refs['parent'].getBoundingClientRect()
+      let p = this.boundingClientRect
       let overRight = this.hovered.x + 150 - p.width > 0
       let overLeft = this.hovered.x - 150 < 0
       return {
@@ -76,12 +92,6 @@ export default {
         }px`,
         top: `${this.hovered.y}px`
       }
-    },
-    animateCanvas: function() {
-      return throttle(this.rawAnimateCanvas, 16)
-    },
-    _mouseout: function() {
-      return debounce(this._rawMouseout, 0, { trailing: true, leading: false })
     }
   },
   watch: {
@@ -113,27 +123,39 @@ export default {
         value: Math.floor(Math.random() * 100)
       }
     })
+
+    this.boundingClientRect = this.$refs['parent'].getBoundingClientRect()
   },
   updated() {
     if (!this.chart) this.createChart()
   },
   beforeDestroy() {
     clearTimeout(this.loadingInterval)
-    window.onresize = null
+    window.removeEventListener('resize', this.resizeChart)
   },
   methods: {
-    _click(event) {
+    calcHeight(d) {
+      if (d.ignore) return this.barStart / 3
+      let height = this.y(0) - this.y(d[this.computedYField])
+      return height > this.min ? height : this.min
+    },
+    calcY(d) {
+      if (d.ignore) return this.barStart / 1.5
+      let _y = this.y(d[this.computedYField])
+      return _y < this.barStart - this.min ? _y : this.barStart - this.min
+    },
+    click({ currentTarget }) {
       // Turn this on for debugging bars more easily
       // console.log(
       //   d,
       //   this.bars.find(b => b.id == d.id)
       // )
-      this.$emit('bar-click', event.currentTarget?.__data__)
+      this.$emit('bar-click', currentTarget?.__data__)
     },
-    _mouseover(event) {
-      const d = event.currentTarget?.__data__
+    mouseover({ currentTarget }) {
+      const d = currentTarget?.__data__
 
-      this._mouseout.cancel()
+      this.computedMouseout.cancel()
       this.hovered = {
         data: d,
         x: this.x(d.id),
@@ -147,7 +169,7 @@ export default {
 
       this.animateCanvas()
     },
-    _rawMouseout() {
+    rawMouseout() {
       this.$emit('bar-mouseout', this.hovered)
       this.hovered = null
       this.hoveredId = null
@@ -167,16 +189,7 @@ export default {
         .append('g')
         .attr('class', 'breaklines-group')
 
-      this.resizeChart()
-
-      const resizeChart = this.resizeChart
-
-      window.addEventListener(
-        'resize',
-        debounce(function() {
-          requestAnimationFrame(resizeChart)
-        }, 150)
-      )
+      window.addEventListener('resize', this.resizeChart)
 
       requestAnimationFrame(this.resizeChart)
     },
@@ -186,7 +199,7 @@ export default {
       const barWidth = this.x.bandwidth()
 
       // Check our current data against existing bars
-      this._items.forEach((item, i) => {
+      this.computedItems.forEach((item, i) => {
         const barIndex = this.bars.findIndex(b => b.id == item.id)
 
         const height1 = this.calcHeight(item)
@@ -232,7 +245,7 @@ export default {
 
       // Check our existing bars against current data
       this.bars.forEach((bar, i) => {
-        const itemExists = this._items.find(item => item.id == bar.id)
+        const itemExists = this.computedItems.find(item => item.id == bar.id)
 
         // If this is a valid bar, do nothing since its data was already
         // updated in the loop above...
@@ -335,7 +348,7 @@ export default {
 
       context.restore()
     },
-    resizeChart() {
+    rawResizeChart() {
       let parent = this.chart.select(function() {
         return this.parentNode
       })
@@ -352,6 +365,8 @@ export default {
         paddingBottom = parseFloat(
           computedStyle.getPropertyValue('padding-bottom')
         )
+
+      this.boundingClientRect = this.$refs['parent'].getBoundingClientRect()
 
       this.chartWidth = this.vertical
         ? this.width
@@ -491,14 +506,10 @@ export default {
         return
       }
 
-      if (!this._items) return
+      if (!this.computedItems) return
       if (!this.loading) clearTimeout(this.loadingInterval)
 
-      const barStart = this.chartHeight - this.chartHeight * 0.1
-
-      this.barStart = barStart
-
-      let domainItems = this._items.map((d, i) => (d.id ? d.id : i))
+      let domainItems = this.computedItems.map((d, i) => (d.id ? d.id : i))
 
       let domain = this.minBands
         ? domainItems.length > this.minBands
@@ -520,7 +531,7 @@ export default {
 
       this.x = x
 
-      let yMax = d3.max(this._items, d => d[this._yField])
+      let yMax = d3.max(this.computedItems, d => d[this.computedYField])
 
       let y
 
@@ -530,34 +541,16 @@ export default {
         y = d3.scaleLinear()
       }
 
-      y.range([barStart, this.chartHeight * 0.2]).domain([
+      y.range([this.barStart, this.chartHeight * 0.2]).domain([
         0,
         this.loading ? 100 : yMax > 0 ? yMax : 100
       ])
 
       this.y = y
 
-      const min = this.chartHeight * 0.15
-
-      const calcHeight = d => {
-        if (d.ignore) return barStart / 3
-        let height = y(0) - y(d[this._yField])
-        return height > min ? height : min
-      }
-
-      this.calcHeight = calcHeight
-
-      const calcY = d => {
-        if (d.ignore) return barStart / 1.5
-        let _y = y(d[this._yField])
-        return _y < barStart - min ? _y : barStart - min
-      }
-
-      this.calcY = calcY
-
       this.groupClickArea
         .selectAll('.click-area-rect')
-        .data(this.loading ? [] : this._items)
+        .data(this.loading ? [] : this.computedItems)
         .join(
           enter =>
             enter
@@ -567,28 +560,28 @@ export default {
               .attr('class', 'click-area-rect')
               .attr('id', d => `click-area-${d.id}`)
               .style('fill', 'transparent')
-              .attr('height', barStart)
+              .attr('height', this.barStart)
               .attr('width', x.bandwidth())
               .attr('y', 0)
               .attr('x', (d, i) => x(d.id ? d.id : i))
               .call(enter =>
                 enter
-                  .on('click', this._click)
-                  .on('mouseout', this._mouseout)
-                  .on('mouseover', this._mouseover)
+                  .on('click', this.click)
+                  .on('mouseout', this.computedMouseout)
+                  .on('mouseover', this.mouseover)
               ),
           update =>
             update
               .attr('id', d => `click-area-${d.id}`)
-              .attr('height', barStart)
+              .attr('height', this.barStart)
               .attr('width', x.bandwidth())
               .attr('y', 0)
               .attr('x', (d, i) => x(d.id ? d.id : i))
               .call(update =>
                 update
-                  .on('click', this._click)
-                  .on('mouseout', this._mouseout)
-                  .on('mouseover', this._mouseover)
+                  .on('click', this.click)
+                  .on('mouseout', this.computedMouseout)
+                  .on('mouseover', this.mouseover)
               ),
           exit =>
             exit.call(exit =>
@@ -606,7 +599,7 @@ export default {
       // the correct index for the x scale
       this.groupText
         .selectAll('.warning-text')
-        .data(this.loading ? [] : this._items)
+        .data(this.loading ? [] : this.computedItems)
         .join(
           enter =>
             enter
@@ -620,7 +613,7 @@ export default {
               .style('opacity', 0)
               .attr('text-anchor', 'middle')
               .text('timelapse')
-              .attr('y', barStart / 1.5 - 3)
+              .attr('y', this.barStart / 1.5 - 3)
               .attr('x', d => x(d.id) + x.bandwidth() / 2)
               .call(enter =>
                 enter
@@ -660,12 +653,15 @@ export default {
             .attr('class', 'x-axis')
             .attr('stroke', 'rgba(0, 0, 0, 0.12)')
             .attr('stroke-width', 1)
-            .attr('d', `M0,${barStart}L0,${barStart}`)
+            .attr('d', `M0,${this.barStart}L0,${this.barStart}`)
             .call(enter =>
               enter
                 .transition('enter')
                 .duration(this.animationDuration)
-                .attr('d', `M0,${barStart}L${this.chartWidth},${barStart}`)
+                .attr(
+                  'd',
+                  `M0,${this.barStart}L${this.chartWidth},${this.barStart}`
+                )
             )
         )
 
