1213c0076ab599c039fba9a861e1becf5b245d18
Jenny <jenny@prefect.io>
2020-09-11 11:25:53 -0400

    add correctly failing tests for flow store

---
+++ tests/unit/store/flow.spec.js
@@ -0,0 +1,54 @@
+import flow from '@/store/flow'
+import { createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('Flow Vuex Module', () => {
+  const initialFlowState = () => {
+    return {
+      flows: null,
+      labels: {}
+    }
+  }
+
+  describe('initial state', () => {
+    it('should have no flows', () => {
+      const state = flow.state
+      expect(state.flows).toBe(null)
+    })
+    it('should have no labels', () => {
+      const state = flow.state
+      expect(state.labels).toEqual({})
+    })
+  })
+
+  describe('getters', () => {
+    let store = new Vuex.Store({
+      state: initialFlowState(),
+      getters: flow.getters
+    })
+
+    it('should return updated labels when the updatedLabels getter is called', () => {
+      expect(store.getters.updatedLabels).toEqual({})
+    })
+  })
+
+  describe('mutationss', () => {
+    let store = new Vuex.Store({
+      state: initialFlowState(),
+      getters: flow.getters,
+      mutations: flow.mutations
+    })
+
+    it('should add new temporary labels when the setTempLabels mutation is called', () => {
+      expect(store.getters.updatedLabels).toEqual({})
+      store.commit('setTempLabels', { flowId: '12345', newLabels: ['a', 'b'] })
+      expect(store.getters.updatedLabels).toEqual({
+        flowId: '12345',
+        newLabels: [1, 2, 3]
+      })
+    })
+  })
+})
