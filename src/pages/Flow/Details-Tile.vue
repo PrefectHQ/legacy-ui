145a300c8f07e88c0355f52b899fce4af3e31b4c
Jenny G <40272060+zhen0@users.noreply.github.com>
2020-09-16 09:55:44 -0400

    Update src/pages/Flow/Details-Tile.vue
    Co-authored-by: Nicholas Brown <nicholas@prefect.io>

--- src/pages/Flow/Details-Tile.vue
+++ src/pages/Flow/Details-Tile.vue
@@ -522,7 +522,7 @@ export default {
                       </div>
                     </template>
                     <span v-if="role == 'READ_ONLY_USER'">
-                      Read-only users can not edit labels</span
+                      Read-only users cannot edit labels</span
                     >
                     <span v-else>Add a label</span>
                   </v-tooltip>
