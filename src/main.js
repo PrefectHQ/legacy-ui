55d120d64f15084a2757a0f8cf54999556e392a1
nicholas <znicholasbrown@gmail.com>
2020-09-16 20:52:58 -0400

    Watch for redirects on app component

--- src/main.js
+++ src/main.js
@@ -117,7 +117,8 @@ Vue.use(Router)
 
 // Vue Global Error Handler
 Vue.config.errorHandler = function(error, vm, trace) {
-  if (error?.message.includes("Cannot read property '_observe' of null")) return
+  if (error?.message?.includes("Cannot read property '_observe' of null"))
+    return
   if (process.env.NODE_ENV === 'development')
     // eslint-disable-next-line no-console
     console.log('Vue Global Error Handler', { error, vm, trace })
@@ -126,7 +127,8 @@ Vue.config.errorHandler = function(error, vm, trace) {
 }
 
 Vue.config.warnHandler = function(error, vm, trace) {
-  if (error?.message.includes("Cannot read property '_observe' of null")) return
+  if (error?.message?.includes("Cannot read property '_observe' of null"))
+    return
   if (process.env.NODE_ENV === 'development')
     // eslint-disable-next-line no-console
     console.log('Vue Global Warn Handler', { error, vm, trace })
