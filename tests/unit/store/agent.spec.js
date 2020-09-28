3828740a2a832c439f360f3cb0b3ebe6cc079817
Jenny <jenny@prefect.io>
2020-09-28 17:29:08 -0400

    add back broken agent test - fix in flow nav guard pr

--- tests/unit/store/agent.spec.js
+++ tests/unit/store/agent.spec.js
@@ -1,66 +1,66 @@
-// import agent from '@/store/agent'
-// import { createLocalVue } from '@vue/test-utils'
-// import Vuex from 'vuex'
+import agent from '@/store/agent'
+import { createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 
-// const localVue = createLocalVue()
-// localVue.use(Vuex)
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
-// describe('Agent Vuex Module', () => {
-//   const initialAgentState = () => {
-//     return {
-//       thresholds: {
-//         // Time before an agent becomes stale
-//         stale: 1, // minutes since last query
-//         // Time before an agent becomes unhealthy
-//         unhealthy: 5 // minutes since last query
-//       },
-//       agents: null
-//     }
-//   }
+describe('Agent Vuex Module', () => {
+  const initialAgentState = () => {
+    return {
+      thresholds: {
+        // Time before an agent becomes stale
+        stale: 1, // minutes since last query
+        // Time before an agent becomes unhealthy
+        unhealthy: 5 // minutes since last query
+      },
+      agents: null
+    }
+  }
 
-//   describe('initial state', () => {
-//     it('should have a stale threshold and unhealthy threshold', () => {
-//       const state = agent.state
-//       expect(state.thresholds.stale).toBe(1)
-//       expect(state.thresholds.unhealthy).toBe(5)
-//     })
-//     it('should have no agents', () => {
-//       const state = agent.state
-//       expect(state.agents).toBe(null)
-//     })
-//   })
+  describe('initial state', () => {
+    it('should have a stale threshold and unhealthy threshold', () => {
+      const state = agent.state
+      expect(state.thresholds.stale).toBe(1)
+      expect(state.thresholds.unhealthy).toBe(5)
+    })
+    it('should have no agents', () => {
+      const state = agent.state
+      expect(state.agents).toBe(null)
+    })
+  })
 
-//   describe('getters', () => {
-//     let store = new Vuex.Store({
-//       state: initialAgentState(),
-//       getters: agent.getters
-//     })
+  describe('getters', () => {
+    let store = new Vuex.Store({
+      state: initialAgentState(),
+      getters: agent.getters
+    })
 
-//     it('should return the stale threshold when the staleThreshold getter is called', () => {
-//       expect(store.getters.staleThreshold).toBe(store.state.thresholds.stale)
-//     })
+    it('should return the stale threshold when the staleThreshold getter is called', () => {
+      expect(store.getters.staleThreshold).toBe(store.state.thresholds.stale)
+    })
 
-//     it('should return the unhealthy threshold when the unhealthyThreshold getter is called', () => {
-//       expect(store.getters.unhealthyThreshold).toBe(
-//         store.state.thresholds.unhealthy
-//       )
-//     })
-//     it('should return agents when the agents getters is called', () => {
-//       expect(store.getters.agents).toBe(null)
-//     })
-//   })
+    it('should return the unhealthy threshold when the unhealthyThreshold getter is called', () => {
+      expect(store.getters.unhealthyThreshold).toBe(
+        store.state.thresholds.unhealthy
+      )
+    })
+    it('should return agents when the agents getters is called', () => {
+      expect(store.getters.agents).toBe(null)
+    })
+  })
 
-//   describe('mutationss', () => {
-//     let store = new Vuex.Store({
-//       state: initialAgentState(),
-//       getters: agent.getters,
-//       mutations: agent.mutations
-//     })
+  describe('mutationss', () => {
+    let store = new Vuex.Store({
+      state: initialAgentState(),
+      getters: agent.getters,
+      mutations: agent.mutations
+    })
 
-//     it('should add new agents when the setAgents mutation is called', () => {
-//       expect(store.getters.agents).toBe(null)
-//       store.commit('setAgents', { id: '12345' })
-//       expect(store.getters.agents).toEqual({ id: '12345' })
-//     })
-//   })
-// })
+    it('should add new agents when the setAgents mutation is called', () => {
+      expect(store.getters.agents).toBe(null)
+      store.commit('setAgents', { id: '12345' })
+      expect(store.getters.agents).toEqual({ id: '12345' })
+    })
+  })
+})
