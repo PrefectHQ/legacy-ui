dd96eedfc8b002e8149c45d3c4945366fb6f7a8b
Jenny <jenny@prefect.io>
2020-10-01 11:53:03 -0400

    allow urls without http protocol

--- src/components/CloudHookForm.vue
+++ src/components/CloudHookForm.vue
@@ -104,7 +104,7 @@ export default {
         requiredCombo: val =>
           (!!val && val.length > 0) || 'At least one number is required',
         url: val => {
-          const pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
+          const pattern = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
           return pattern.test(val) || 'Invalid URL.'
         }
       },
