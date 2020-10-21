972a7754a8bda4879f01ef030ed087c0598cccf2
Jenny G <40272060+zhen0@users.noreply.github.com>
2020-10-21 12:37:38 -0400

    Update src/pages/Onboard/AcceptInvitationPage.vue
    Co-authored-by: Nicholas Brown <nicholas@prefect.io>

--- src/pages/Onboard/AcceptInvitationPage.vue
+++ src/pages/Onboard/AcceptInvitationPage.vue
@@ -35,7 +35,7 @@ export default {
     },
     errorMessage() {
       if (this.membershipInvitation && !this.membershipInvitation.user)
-        return "The membership ID provided doesn't match this user. Are you in the right account? "
+        return "This invitation appears to be for someone else - did you log in to the correct account? "
       if (this.error)
         return 'It looks like there was a problem with your invitation.  Please check the details and your account and try again.'
       return `We can't find your membership invitation. Have you already
