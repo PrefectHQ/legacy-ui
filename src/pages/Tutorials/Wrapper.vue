74663e58ca0077f02a9decfc4b00ac406f6e3da0
Natalie Smith <thatgalnatalie@gmail.com>
2020-11-06 13:30:23 -0800

    Revert "Revert "added md routes/toc for the tutorials""

---
+++ src/pages/Tutorials/Wrapper.vue
@@ -0,0 +1,16 @@
+<script>
+import { parser } from '@/utils/markdownParser'
+export default {
+  methods: {
+    fileMD() {
+      return parser(require(`./Markdown/${this.$route.name}.md`))
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div v-html="fileMD()"></div>
+  </div>
+</template>
