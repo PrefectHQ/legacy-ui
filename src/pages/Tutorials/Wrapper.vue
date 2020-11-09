263eb1c8e2fbfb0b5432b487ddaa11b2a689492f
Natalie Smith <thatgalnatalie@gmail.com>
2020-11-09 14:41:20 -0800

    added some plugins to see if it fixes the md rendering issue

--- src/pages/Tutorials/Wrapper.vue
+++ src/pages/Tutorials/Wrapper.vue
@@ -1,9 +1,12 @@
 <script>
-import { parser } from '@/utils/markdownParser'
+import { parser, mdParser } from '@/utils/markdownParser'
 export default {
   methods: {
     fileMD() {
       return parser(require(`./Markdown/${this.$route.name}.md`))
+    },
+    otherMD() {
+      return mdParser(require(`./Markdown/${this.$route.name}.md`)).contents
     }
   }
 }
@@ -11,6 +14,6 @@ export default {
 
 <template>
   <div>
-    <div v-html="fileMD()"></div>
+    <div v-html="otherMD()"></div>
   </div>
 </template>
