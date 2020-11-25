f13fc43d2b5bb7c1c1d27b7a910bb3cdf6a957d5
Allyson <allyson@prefect.io>
2020-11-25 18:07:47 -0500

    Sub page nav (#441)
    * move tabs into their own component
    
    * make navtabbar logic more general, for use in all subnavs
    
    * use navtabbar in flow page (also cleans up some dashboard code)
    
    * remove extraneous commented-out stuff
    
    * use navtabbar in flowrun page (also cleans up some flow page code)
    
    * use navtabbar in taskrun page
    
    * use navtabbar in task page
    
    * add ability to mark buttons as disabled
    
    * add ability to hide buttons
    
    * add badge functionality, fix mapped runs button logic
    
    * add explanation card to 'coming soon' badge
    
    * add right-aligned tab functionality, make tab bar full-width
    
    * remove unused file
    
    * initial changes to stick subnav to top of page
    
    * styling
    
    * changelog
    
    * better styling of subnav elevations
    
    * move page type to side, add icon
    
    * padding tweaks
    
    * make everything look good on mobile/small sizes
    
    * fix position of page type icon
    
    * fix styling of page type
    
    * have card show when hovering over entire tab
    
    * Use standard prop instead of slot
    
    * Correctly constrain version selector width
    
    * Fix flow run editing height
    
    * Add breadcrumbs for projects and tenant
    
    * Fix centering on flow run actions
    
    * Fix actions centering on flow and task run pages as well
    
    * Improve project-selector alignment on small screens
    
    * Fix padding on flow action bar
    
    * Bring project selector design in line with other action selectors
    
    * Fix bug where tabs weren't correctly selected when linking from certain places
    
    * Fix padding on dashboard
    
    * Fix issue with vertical rhythm
    
    * Show nav shadow on scroll
    
    * Fix notifications layout padding
    
    * Improve hover experience for editable text fiel
    
    * Fix height of a few action bar buttons
    
    * Fix offset nav tabs
    
    Co-authored-by: nicholas <znicholasbrown@gmail.com>
    Co-authored-by: Nicholas Brown <nicholas@prefect.io>

--- src/layouts/NotificationsLayout.vue
+++ src/layouts/NotificationsLayout.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-container style="max-width: 1440px;" class="pt-0 pb-12">
+  <v-container
+    style="
+      max-width: 1440px;
+      padding-top: 60px;
+    "
+    class="pb-12"
+  >
     <v-row class="mt-0">
       <v-col v-if="$slots['col-1']" cols="12" sm="4" md="3">
         <slot name="col-1" />
