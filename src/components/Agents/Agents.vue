2c9aa28720df6e2c0371d927cce56e3870b138d7
kingsley <kingsley.blatter@gmail.com>
2020-12-02 11:02:05 -0500

    updating path direct to universal-deploy

--- src/components/Agents/Agents.vue
+++ src/components/Agents/Agents.vue
@@ -431,7 +431,7 @@ export default {
         to understand their role in your infrastructure.
         <span v-if="isCloud">
           You can also go through the
-          <router-link class="link" :to="{ name: 'tutorial' }">
+          <router-link class="link" :to="{ path: 'tutorial/Universal-Deploy' }">
             <u>Universal Deploy</u>
           </router-link>
           tutorials for guidance on running agents in Cloud.</span
