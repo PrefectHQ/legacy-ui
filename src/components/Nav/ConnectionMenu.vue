018da5407058d79b9b4955583d65f848c048f605
nicholas <znicholasbrown@gmail.com>
2020-12-17 15:08:34 -0500

    Add handlers for switching backends

--- src/components/Nav/ConnectionMenu.vue
+++ src/components/Nav/ConnectionMenu.vue
@@ -1,16 +1,53 @@
 <script>
-import { mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 import ExternalLink from '@/components/ExternalLink'
+import { clearCache } from '@/vue-apollo'
+
+const tenantProtectedRoutes = [
+  'project',
+  'flow',
+  'flow-run',
+  'task',
+  'task-run',
+  'flow-run-logs',
+  'select-plan',
+  'billing',
+  'welcome',
+  'current-plan',
+  'payment',
+  'api',
+  'name-team'
+]
+
+const backendProtectedRoutes = [
+  'team',
+  'account',
+  'task-concurrency',
+  'members',
+  'secrets',
+  'tokens',
+  'user',
+  'profile',
+  'user-tokens',
+  'welcome',
+  'name-team',
+  'onboard-resources'
+]
 
 export default {
   components: { ExternalLink },
   data() {
     return {
-      model: false
+      backendTimeout: null,
+      disabled: false,
+      loading: false,
+      model: false,
+      value: this.backend
     }
   },
   computed: {
     ...mapGetters('api', [
+      'backend',
       'isServer',
       'isCloud',
       'url',
@@ -23,6 +60,63 @@ export default {
       if (this.connected) return ['connected']
       if (this.connecting) return ['connecting', 'connecting-animate']
       return ['disconnected']
+    },
+    statusColor() {
+      if (this.connected) return '#06e797'
+      if (this.connecting) return 'var(--v-warning-base)'
+      return 'var(--v-Failed-base)'
+    }
+  },
+  beforeDestroy() {
+    clearTimeout(this.backendTimeout)
+  },
+  methods: {
+    ...mapActions('api', ['switchBackend']),
+    async _switchBackend() {
+      clearTimeout(this.backendTimeout)
+      this.loading = true
+      this.disabled = true
+
+      this.backendTimeout = setTimeout(async () => {
+        if (!this.isCloud) {
+          await this.switchBackend('CLOUD')
+        } else {
+          await this.switchBackend('SERVER')
+        }
+
+        this.loading = false
+        this.disabled = false
+
+        clearCache()
+        this.handlePostTokenRouting()
+      }, 1000)
+    },
+    handlePostTokenRouting() {
+      if (this.isCloud && !this.tenant.settings.teamNamed) {
+        this.$router.push({
+          name: 'welcome',
+          params: {
+            tenant: this.tenant.slug
+          }
+        })
+
+        return
+      }
+
+      if (
+        tenantProtectedRoutes.includes(this.$route.name) ||
+        (this.isServer && backendProtectedRoutes.includes(this.$route.name))
+      ) {
+        this.$router.push({
+          name: 'dashboard',
+          params: { tenant: this.tenant.slug }
+        })
+      } else {
+        this.$router.push({
+          name: this.$route.name,
+          params: { ...this.$route.params, tenant: this.tenant.slug }
+        })
+      }
     }
   }
 }
@@ -34,46 +128,9 @@ export default {
     :close-on-content-click="false"
     offset-y
     transition="slide-y-transition"
-    nudge-bottom="15"
+    nudge-bottom="20"
   >
     <template #activator="{ on }">
-      <!-- <v-btn
-        class="ml-2 position-relative"
-        text
-        icon
-        large
-        color="white"
-        v-on="on"
-      >
-        <v-icon>
-          {{ connectedIcon }}
-        </v-icon>
-        <v-icon
-          v-if="connecting"
-          small
-          color="primary"
-          class="position-absolute"
-          :style="{
-            bottom: '-8px',
-            right: '0'
-          }"
-        >
-          fas fa-spinner fa-pulse
-        </v-icon>
-        <v-icon
-          v-if="apiMode == 'maintenance'"
-          small
-          color="accentPink"
-          class="position-absolute"
-          :style="{
-            bottom: '-8px',
-            right: '0'
-          }"
-        >
-          fas fa-exclamation
-        </v-icon>
-      </v-btn> -->
-
       <v-btn class="navbar-icon mx-1" :class="buttonClass" icon v-on="on">
         <i class="fad fa-server fa-2x nav-bar-duotone-icon" />
       </v-btn>
@@ -85,10 +142,10 @@ export default {
         colored-border
         icon="fad fa-clouds"
         tile
-        color="primary"
-        class="text-body-1"
+        :color="statusColor"
+        class="text-body-1 mb-0"
       >
-        <div class="mb-2">
+        <div class="mb-2 text-h6 font-weight-light">
           <span v-if="connected">Connected</span>
           <span v-else-if="connecting">Connecting</span>
           <span v-else>Couldn't connect</span>
@@ -149,102 +206,33 @@ export default {
           new runs will be released to your Agents and state updates may be
           delayed.
         </div>
+
+        <div v-else-if="isCloud">
+          Having trouble? Check out the
+          <ExternalLink
+            href="https://github.com/PrefectHQ/prefect/discussions/category_choices"
+            >Prefect Cloud status page</ExternalLink
+          >
+          for information about upcoming maintenance windows and the current
+          uptime of Prefect Cloud services.
+        </div>
+
+        <div class="mt-4">
+          Cloud
+          <v-switch
+            v-model="backend"
+            color="primary"
+            :disabled="disabled"
+            :loading="loading"
+            inset
+            dense
+            hide-details
+            @change="_switchBackend"
+          />
+          Server
+        </div>
       </v-alert>
     </v-sheet>
-
-    <!-- <v-card
-      tile
-      class="pa-0"
-      max-width="700"
-      :min-width="
-        (isServer && (!connected || connecting)) || apiMode == 'maintenance'
-          ? 600
-          : 300
-      "
-    >
-      <v-card-text class="pb-0">
-        <div class="mb-2">
-          <span v-if="connected">Connected</span>
-          <span v-else-if="connecting">Connecting</span>
-          <span v-else>Couldn't connect</span>
-          to
-          <span class="font-weight-bold">
-            <span v-if="isCloud" class="primary--text">Prefect Cloud</span>
-            <span v-else class="secondaryGray--text">Prefect Server</span>
-          </span>
-          <span v-if="isServer">
-            at
-            <span class="font-weight-bold">{{ url }}</span>
-          </span>
-
-          <v-alert
-            v-if="isServer && (!connected || connecting)"
-            border="left"
-            colored-border
-            class="text-body-1 mt-2"
-            icon="cloud"
-            color="primary"
-            tile
-          >
-            <div>
-              Having trouble?
-              <span class="font-weight-medium">Don't panic! </span>
-            </div>
-            <div class="mt-4">
-              The <v-icon x-small>fab fa-slack</v-icon>&nbsp;
-              <ExternalLink
-                href="https://join.slack.com/t/prefect-community/shared_invite/enQtODQ3MTA2MjI4OTgyLTliYjEyYzljNTc2OThlMDE4YmViYzk3NDU4Y2EzMWZiODM0NmU3NjM0NjIyNWY0MGIxOGQzODMxNDMxYWYyOTE"
-                >Prefect Slack community</ExternalLink
-              >
-              is a great place to ask questions, provide feedback, or just to
-              chat! Check out the
-              <ExternalLink
-                href="https://docs.prefect.io/orchestration/server/overview.html#what-is-prefect-server"
-                >Prefect documentation</ExternalLink
-              >
-
-              for tips on setting up Prefect Server, idioms for writing flows,
-              and so much more. Looking for more in-depth discussion? Our
-              <ExternalLink
-                href="https://github.com/PrefectHQ/prefect/discussions/category_choices"
-                >GitHub Discussion board</ExternalLink
-              >
-              is a great place to present long-form ideas, technical challenges,
-              or to show off your Prefect tasks and flows!
-
-              <div class="mt-4"
-                >Did you know that
-
-                <ExternalLink href="https://www.prefect.io/cloud"
-                  >Prefect Cloud</ExternalLink
-                >
-                offers a feature-rich and fully-managed orchestration layer for
-                your flows? Oh and it's also <em>free</em>.
-              </div>
-            </div>
-          </v-alert>
-        </div>
-        <v-alert
-          v-if="apiMode == 'maintenance'"
-          border="left"
-          colored-border
-          class="text-body-1"
-          type="warning"
-          color="accentPink"
-          tile
-        >
-          Prefect Cloud is undergoing routine maintenance; during this time no
-          new runs will be released to your Agents and state updates may be
-          delayed.
-        </v-alert>
-      </v-card-text>
-      <v-card-actions class="pt-0">
-        <v-spacer></v-spacer>
-        <v-btn small text @click="model = false">
-          Close
-        </v-btn>
-      </v-card-actions>
-    </v-card> -->
   </v-menu>
 </template>
 
