b8f6e81cb8c7c6076aac87b750adc11059b74bfb
Natalie Smith <thatgalnatalie@gmail.com>
2021-01-04 16:45:53 -0800

    dismiss button placement

--- src/components/TutorialBanner.vue
+++ src/components/TutorialBanner.vue
@@ -21,7 +21,7 @@ export default {
       let show = this.flows?.length === 0
       if (!this.dismissed && this.pageScroll) {
         show = !this.pageScroll
-      } else if (this.dismissed) {
+      } else if (this.dismissed || this.$vuetify.breakpoint.smAndDown) {
         show = false
       }
       return show
@@ -32,31 +32,35 @@ export default {
 
 <template>
   <v-banner :value="showBanner">
-    <v-avatar slot="icon" color="#3b8dff" size="40">
-      <v-icon icon="mdi-lock" color="white">
-        school
-      </v-icon>
-    </v-avatar>
-    Pst! It looks like you don't have any flows yet; check out the
-    <router-link :to="{ name: 'tutorial' }">
-      tutorials
-    </router-link>
-    for walkthroughs on writing and registering flows with Prefect and the
-    <ExternalLink href="https://docs.prefect.io/">documentation</ExternalLink>
-    for more in-depth looks at the Prefect APIs
-
-    <template #actions>
-      <v-btn
-        text
-        dark
-        color="#ff5252"
-        class="mb-3"
-        @click="dismissed = !dismissed"
-      >
-        <v-icon>
-          close
-        </v-icon>
-      </v-btn>
+    <template #default>
+      <div class="d-inline-flex align-center">
+        <v-avatar slot="icon" color="#3b8dff" size="35">
+          <v-icon icon="mdi-lock" color="white">
+            school
+          </v-icon>
+        </v-avatar>
+        <div class="ml-3">
+          Pst! It looks like you don't have any flows yet; check out the
+          <router-link :to="{ name: 'tutorial' }"> tutorials</router-link> for
+          walkthroughs on writing and registering flows with Prefect and the
+          <ExternalLink href="https://docs.prefect.io/"
+            >documentation</ExternalLink
+          >
+          for more in-depth looks at the Prefect APIs
+        </div>
+        <v-btn
+          class="mr-3"
+          icon
+          text
+          dark
+          color="#ff5252"
+          @click="dismissed = !dismissed"
+        >
+          <v-icon>
+            close
+          </v-icon>
+        </v-btn>
+      </div>
     </template>
   </v-banner>
 </template>
