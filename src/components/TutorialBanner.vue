1a1d6fc8c8f987ec7eafbda9ac4a0a30519b353d
Natalie Smith <thatgalnatalie@gmail.com>
2021-01-04 17:20:08 -0800

    increased dismiss btn margin

--- src/components/TutorialBanner.vue
+++ src/components/TutorialBanner.vue
@@ -33,13 +33,13 @@ export default {
 <template>
   <v-banner :value="showBanner">
     <template #default>
-      <div class="d-inline-flex align-center">
+      <div class="d-flex align-center">
         <v-avatar slot="icon" color="#3b8dff" size="35">
           <v-icon icon="mdi-lock" color="white">
             school
           </v-icon>
         </v-avatar>
-        <div class="ml-3">
+        <div class="ml-4">
           Pst! It looks like you don't have any flows yet; check out the
           <router-link :to="{ name: 'tutorial' }"> tutorials</router-link> for
           walkthroughs on writing and registering flows with Prefect and the
@@ -49,7 +49,7 @@ export default {
           for more in-depth looks at the Prefect APIs
         </div>
         <v-btn
-          class="mr-3"
+          class="mr-2 mb-3"
           icon
           text
           dark
