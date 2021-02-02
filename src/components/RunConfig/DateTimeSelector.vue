1c93a54e5f5d5aaf17eb21b69539fc9e741a15ea
nicholas <znicholasbrown@gmail.com>
2021-02-01 22:35:09 -0500

    Clean up datetime selector

--- src/components/RunConfig/DateTimeSelector.vue
+++ src/components/RunConfig/DateTimeSelector.vue
@@ -10,18 +10,6 @@ export default {
       required: false,
       default: () => true
     },
-    // Specify a warning to display if the selected time is in the past.
-    warning: {
-      type: String,
-      required: false,
-      default: () => 'This time is in the past.'
-    },
-    // Custom props for the read-only text field displaying the selected date and time.
-    textFieldProps: {
-      type: Object,
-      required: false,
-      default: () => ({})
-    },
     // Two-way-bound date & time.
     // Format: ISO 8601, e.g. 2020-03-18T15:30:32-05:00
     // This prop is required for use of v-model on this component.
@@ -37,16 +25,9 @@ export default {
       dateTime: this.momentWithTimezone(),
       dateInterval: null,
 
-      timeHr: '12',
-      timeMin: '00',
-      timeAmPm: 'am',
-
       // Get either the user-set timezone or the resolved local timezone
       timezone_:
         this.timezone || Intl.DateTimeFormat().resolvedOptions().timeZone,
-
-      // Current date & time
-      currentDateTimeMoment: null,
 
       // These are set to true if a user has edited the date/time fields
       // so that the counter doesn't continue to increment values
@@ -207,8 +188,8 @@ export default {
 <template>
   <v-container fluid>
     <v-row no-gutters>
-      <v-col cols="12" md="5" class="d-flex align-center justify-center">
-        <div class="pb-16 text-h5 text-center">
+      <v-col cols="12" md="5" class="d-flex align-center justify-space-between">
+        <div class="pb-16 text-h5 mx-auto">
           <div>
             {{ displayMonth }}
             <span class="primary--text"> {{ displayDay }}</span
@@ -223,7 +204,7 @@ export default {
         </div>
       </v-col>
 
-      <v-col cols="12" md="1">
+      <v-col cols="12" md="1" class="text-center">
         <v-divider :vertical="$vuetify.breakpoint.mdAndUp" />
       </v-col>
 
@@ -283,124 +264,8 @@ export default {
               @input="handleChangeTime"
             />
           </v-col>
-          <!-- <v-col cols="4">
-            
-          </v-col>
-          <v-col cols="4">
-            
-          </v-col> -->
         </v-row>
       </v-col>
     </v-row>
-    <!-- <v-menu
-      v-model="showMenu"
-      max-width="290"
-      offset-y
-      :close-on-content-click="false"
-    >
-      <template #activator="{ on }">
-        <v-text-field
-          :value="formattedValue"
-          clearable
-          readonly
-          v-bind="textFieldProps"
-          v-on="on"
-          @click:clear="handleDateTimeClear"
-        ></v-text-field>
-      </template>
-      <v-tabs v-model="tab" grow class="tab-height-custom">
-        <v-tab key="date" href="#date">Date</v-tab>
-        <v-tab key="time" href="#time" :disabled="!date">
-          Time
-        </v-tab>
-
-        <v-tab-item value="date">
-          <v-date-picker
-            v-model="date"
-            no-title
-            :min="currentDate"
-            label="Hour"
-            @change="handleDateChange"
-          ></v-date-picker>
-        </v-tab-item>
-
-        <v-tab-item value="time">
-          <v-card tile>
-            <v-card-text>
-              <v-row>
-                <v-col cols="4">
-                  <v-autocomplete
-                    v-model="timeHr"
-                    hide-details
-                    label="Hour"
-                    :items="timeRange(1, 13)"
-                    :append-icon="null"
-                    hide-no-data
-                  ></v-autocomplete>
-                </v-col>
-                <v-col cols="4">
-                  <v-autocomplete
-                    v-model="timeMin"
-                    hide-details
-                    label="Minute"
-                    :items="timeRange(0, 60)"
-                    :append-icon="null"
-                    hide-no-data
-                  ></v-autocomplete>
-                </v-col>
-                <v-col cols="4">
-                  <v-autocomplete
-                    v-model="timeAmPm"
-                    hide-details
-                    label="AM/PM"
-                    :items="['AM', 'PM']"
-                    :append-icon="null"
-                    hide-no-data
-                  ></v-autocomplete>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="12">
-                  <p>Timezone: {{ timezone || 'Local' }}</p>
-                  <p class="mb-0">
-                    You can change your system's timezone from your user
-                    profile.
-                    <router-link :to="{ name: 'profile' }">
-                      Update timezone
-                    </router-link>
-                    .
-                  </p>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="12" class="py-0">
-                  <v-scroll-x-transition>
-                    <v-alert
-                      v-if="timeInPast"
-                      border="left"
-                      colored-border
-                      elevation="2"
-                      type="warning"
-                      dense
-                      class="mt-4 mb-0"
-                    >
-                      <span class="body-2 ma-0">
-                        {{ warning }}
-                      </span>
-                    </v-alert>
-                  </v-scroll-x-transition>
-                </v-col>
-              </v-row>
-            </v-card-text>
-            <v-card-actions class="pa-0">
-              <v-spacer></v-spacer>
-              <v-btn text color="primary" @click="showMenu = false">
-                Confirm
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-tab-item>
-      </v-tabs>
-    </v-menu> -->
   </v-container>
 </template>
