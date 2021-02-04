072938f7a3af976eda0212e4d2584be51c888d84
Jenny <jenny@prefect.io>
2021-02-04 11:18:34 -0500

    update banner to change wording for server

--- src/components/TutorialBanner.vue
+++ src/components/TutorialBanner.vue
@@ -19,14 +19,14 @@ export default {
     ...mapGetters('data', ['flows']),
     ...mapGetters('api', ['isCloud']),
     showBanner() {
-      if (!this.isCloud) return false
-      let show = this.flows?.length === 0
-      if (!this.dismissed && this.pageScroll) {
-        show = !this.pageScroll
-      } else if (this.dismissed || this.$vuetify.breakpoint.smAndDown) {
-        show = false
-      }
-      return show
+      return true
+      // let show = this.flows?.length === 0
+      // if (!this.dismissed && this.pageScroll) {
+      //   show = !this.pageScroll
+      // } else if (this.dismissed || this.$vuetify.breakpoint.smAndDown) {
+      //   show = false
+      // }
+      // return show
     }
   }
 }
@@ -42,13 +42,19 @@ export default {
           </v-icon>
         </v-avatar>
         <div class="ml-4">
-          Pst! It looks like you don't have any flows yet; check out the
-          <router-link :to="{ name: 'tutorial' }"> tutorials</router-link> for
-          walkthroughs on writing and registering flows with Prefect and the
-          <ExternalLink href="https://docs.prefect.io/"
-            >documentation</ExternalLink
+          Pst! It looks like you don't have any flows yet;
+          <span v-if="isCloud"
+            >check out the
+            <router-link :to="{ name: 'tutorial' }"> tutorials</router-link> for
+            walkthroughs on writing and registering flows with Prefect and the
+            for more in-depth looks at the Prefect APIs</span
+          ><span v-else>
+            check out our
+            <ExternalLink href="https://docs.prefect.io/"
+              >documentation</ExternalLink
+            >
+            for help on getting started.</span
           >
-          for more in-depth looks at the Prefect APIs
         </div>
         <v-btn
           class="ml-auto"
