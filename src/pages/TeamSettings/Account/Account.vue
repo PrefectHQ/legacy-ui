ef858ec0baccd51a70387323e918791d693787b6
nicholas <znicholasbrown@gmail.com>
2021-02-17 21:10:26 -0500

    Add some placeholder ring charts

--- src/pages/TeamSettings/Account/Account.vue
+++ src/pages/TeamSettings/Account/Account.vue
@@ -8,11 +8,13 @@ import License from '@/pages/TeamSettings/Account/License'
 import Users from '@/pages/TeamSettings/Account/Users'
 import Billing from '@/pages/TeamSettings/Account/Billing'
 import ClearDataDialog from '@/pages/TeamSettings/Account/ClearDataDialog'
-import Usage from '@/pages/TeamSettings/Account/Usage/Usage'
+// import Usage from '@/pages/TeamSettings/Account/Usage/Usage'
 import UsageTimeline from '@/pages/TeamSettings/Account/Usage/UsageTimeline'
+import ChartCard from '@/components/ChartCard/ChartCard'
 
 export default {
   components: {
+    ChartCard,
     ManagementLayout,
     Profile,
     Users,
@@ -20,7 +22,7 @@ export default {
     LegacyLicense,
     License,
     Billing,
-    Usage,
+    // Usage,
     UsageTimeline
   },
   mixins: [teamProfileMixin],
@@ -55,31 +57,41 @@ export default {
 
 <template>
   <div>
-    <ManagementLayout>
-      <!-- <template v-if="needAlert && isCloud" #alert>
-      <v-alert
-        dismissible
-        class="mx-auto mb-12"
-        border="left"
-        colored-border
-        elevation="2"
-        type="warning"
-        tile
-        prominent
-        ><div>You are connected to Prefect Cloud. </div
-        ><div>
-          Any changes you make here will affect your Cloud account.</div
-        ></v-alert
-      >
-    </template>
-    <template #title>Account</template>
-
-    <template v-if="isCloud" #subtitle>
-      Manage your team's billing, data, and profile information.
-    </template>
-    <template v-else #subtitle>
-      Update your team's profile.
-    </template> -->
+    <div v-if="isCloud && isUsageBased">
+      <v-row no-gutters class="my-16">
+        <v-col cols="12" md="6">
+          <ChartCard
+            :chart-data="[
+              { label: 'used', value: 3230, color: '#ff8cc6' },
+              { label: 'total', value: 10000, color: '#de369d' }
+            ]"
+            :extra="false"
+            chart-type="ring"
+            :colors="['#fff', 'rgb(59, 141, 255, 0.05)']"
+            accent-color="rgb(59, 141, 255, 1)"
+            chart-overlay-center-type="icon"
+            chart-overlay-main="3230"
+            chart-overlay-units="runs"
+            chart-overlay-center="fad fa-tasks"
+          />
+        </v-col>
+        <v-col cols="12" md="6">
+          <ChartCard
+            :chart-data="[
+              { label: 'used', value: 1, color: '#ff8cc6' },
+              { label: 'total', value: 3, color: '#de369d' }
+            ]"
+            :extra="false"
+            chart-type="ring"
+            :colors="['#fff', 'rgb(59, 141, 255, 0.05)']"
+            accent-color="rgb(59, 141, 255, 1)"
+            chart-overlay-center-type="icon"
+            chart-overlay-main="1"
+            chart-overlay-units="user"
+            chart-overlay-center="fad fa-users"
+          />
+        </v-col>
+      </v-row>
 
       <v-row no-gutters>
         <v-col cols="12">
@@ -87,20 +99,63 @@ export default {
         </v-col>
       </v-row>
 
-      <v-row no-gutters>
-        <v-col cols="12" md="6"> </v-col>
-        <v-col cols="12" md="6">
-          <Usage v-if="isCloud && isUsageBased" />
-        </v-col>
-      </v-row>
+      <Profile />
+      <License />
+      <Billing />
+      <ClearDataDialog />
+    </div>
+
+    <ManagementLayout v-else-if="isCloud">
+      <template v-if="needAlert && isCloud" #alert>
+        <v-alert
+          dismissible
+          class="mx-auto mb-12"
+          border="left"
+          colored-border
+          elevation="2"
+          type="warning"
+          tile
+          prominent
+          ><div>You are connected to Prefect Cloud. </div
+          ><div>
+            Any changes you make here will affect your Cloud account.</div
+          ></v-alert
+        >
+      </template>
+      <template #title>Account</template>
+
+      <template v-if="isCloud" #subtitle>
+        Manage your team's billing, data, and profile information.
+      </template>
+      <template v-else #subtitle>
+        Update your team's profile.
+      </template>
 
       <Profile />
-      <LegacyLicense v-if="isCloud && !isUsageBased" />
-      <License v-if="isCloud && isUsageBased" />
-      <Users v-if="isCloud && !isUsageBased" />
-      <Billing v-if="isCloud" />
-      <ClearDataDialog v-if="isCloud" />
+      <LegacyLicense />
+      <Users />
+      <Billing />
+      <ClearDataDialog />
     </ManagementLayout>
+
+    <div v-else>
+      <v-alert
+        class="mx-auto mb-12"
+        border="left"
+        colored-border
+        elevation="2"
+        type="primary"
+        tile
+        prominent
+        ><div>You are connected to Prefect Server. </div
+        ><div>
+          Account management is only available on
+          <ExternalLink href="https://www.prefect.io/cloud"
+            >Prefect Cloud</ExternalLink
+          >
+        </div>
+      </v-alert>
+    </div>
   </div>
 </template>
 
