7939287aeaa1808c7163d70fc03e3ad464d0232d
Jenny <jenny@prefect.io>
2021-03-08 16:59:11 -0500

    add default messages and begin to add slack and email config

--- src/pages/Dashboard/Actions/AddDoThis.vue
+++ src/pages/Dashboard/Actions/AddDoThis.vue
@@ -2,36 +2,66 @@
 //This page will need updating!
 import { featureFlaggedCloudHookTypes } from '@/utils/cloudHooks'
 import { mapGetters } from 'vuex'
-const date = new Date().toDateString()
 
 export default {
   data() {
     return {
       addName: false,
       tempType: '',
-      messageType: { title: 'send a' },
+      messageType: { title: 'Send' },
       openSendMessage: true,
       openMessageConfig: false,
-      messageConfig: { name: 'a URL' },
+      messageConfig: null,
+      messageConfigTo: '',
       // To do - can we check what the default message will be?
-      messageText: 'the default message',
+      messageName: 'this message',
+      messageText: '',
       openMessageText: false,
       openName: false,
-      newConfigName: '',
+      newSaveAs: '',
       isPagerDuty: false,
-      isTwilio: false
+      isTwilio: false,
+      menu: false,
+      bothMessages: false
     }
   },
   computed: {
     ...mapGetters('api', ['isCloud']),
     ...mapGetters('user', ['user']),
-    configName: {
+    messageConfigLabel() {
+      console.log(this.messageType)
+      return this.messageType?.type === 'EMAIL' ||
+        this.messageType.type === 'WEBHOOK'
+        ? 'Email address(es)'
+        : this.messageType?.type === 'TWILIO'
+        ? 'Twilio Hook'
+        : this.messageType?.type === 'SLACK_WEBHOOK'
+        ? 'What is the name of the secret your slack webhook is stored as?'
+        : 'config details'
+    },
+    messageConfigRules() {
+      return []
+    },
+    saveAs: {
       get() {
-        return `send ${this.messageType.title} created by ${this.user.first_name} on ${date}`
+        return `${this.messageType.title} to ${this.to}`
       },
       set(x) {
-        this.newConfigName = x
+        this.newSaveAs = x
       }
+    },
+    to() {
+      return this.messageType?.type === 'EMAIL' ||
+        this.messageType.type === 'WEBHOOK'
+        ? this.messageType?.config?.to || 'this email address.'
+        : this.messageType?.type === 'TWILIO'
+        ? this.messageType?.config?.to?.toString() || ' this address.'
+        : this.messageType?.type === 'SLACK_WEBHOOK'
+        ? this.messageType?.config?.url || ' this webhook.'
+        : 'who?'
+    },
+    completeConfig() {
+      return this.messageType && this.messageConfig
     }
   },
   methods: {
@@ -42,6 +72,12 @@ export default {
     },
     handleClose() {
       this.$emit('close-action')
+    },
+    saveConfig() {
+      this.messageConfig =
+        this.messageType.type === 'SLACK_WEBHOOK'
+          ? { webhook_url: this.messageConfigTo }
+          : {}
     },
     actionTypes() {
       let allHooks
@@ -95,7 +131,7 @@ export default {
           :color="openMessageText ? 'codePink' : 'grey'"
           @click="openMessageText = !openMessageText"
         >
-          {{ messageText }}</v-btn
+          {{ messageName }}</v-btn
         >
       </span>
       to
@@ -106,13 +142,11 @@ export default {
         :color="openMessageConfig ? 'codePink' : 'grey'"
         @click="openMessageConfig = !openMessageConfig"
       >
-        {{ messageConfig.name }}</v-btn
+        {{ messageConfigTo || to }}</v-btn
       >
       <span v-if="isTwilio"> for</span>
 
       <span v-if="isPagerDuty"> </span>
-
-      .
     </div>
 
     <v-card-text v-if="openSendMessage" class="pt-0">
@@ -132,13 +166,82 @@ export default {
       </v-chip>
     </v-card-text>
     <v-card-text v-if="openMessageText" class="pt-0">
-      <v-text-field v-model="messageText" />
+      <span class="primary--text"
+        >Type your message here or leave blank to send a default message.</span
+      ><v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :open-on-hover="true"
+      >
+        <template #activator="{ on, attrs }">
+          <v-btn icon small v-bind="attrs" v-on="on">
+            <v-icon color="primary" small>info</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card width="30vW">
+          <v-card-text
+            ><div class="mb-2"
+              >The default message varies depending on what type of action you
+              attach the config to.
+            </div>
+            <div>
+              For a state change event the message would be:
+              <span class="font-weight-light"
+                >"Run `{flow_run_name}` of flow `{flow_name}` entered state
+                `{state}` with " "message `{state_message}`. See {flow_run_link}
+                for more details."</span
+              ></div
+            >
+            <div
+              >For a flow SLA event the default message would be:
+              <span class="font-weight-light"
+                >"Run `{flow_run_name}` (`{flow_run_id}`) of flow `{flow_name}`
+                failed `{kind}` " "SLA (`{flow_sla_config_id}`) after
+                {duration_seconds} seconds. " "See {flow_run_link} for more
+                details."</span
+              ></div
+            >
+            <div
+              >For an agent SLA event, the default message would be:
+              <span class="font-weight-light"
+                >"Agents sharing the config {agent_config_id} have failed the
+                minimum healthy " "count of {sla_min_healthy}. The following
+                agents are unhealthy: {agent_ids}"</span
+              ></div
+            ></v-card-text
+          >
+        </v-card>
+      </v-menu>
+      <v-checkbox v-model="bothMessages" class="mt-0 no-top" dense>
+        <template #label
+          ><span class="body-2"
+            >Add this message <span class="font-weight-bold"> and</span> the
+            default message.</span
+          ></template
+        >
+      </v-checkbox>
+      <v-text-field
+        v-model="messageText"
+        class="pt-0"
+        @keydown.enter="openMessageText = false"
+      />
+    </v-card-text>
+    <v-card-text v-else-if="openMessageConfig" v-click-outside="saveConfig()">
+      <v-text-field
+        v-model="messageConfigTo"
+        :label="messageConfigLabel"
+        :rules="messageConfigRules"
+        @keyup.enter="saveConfig()"
+      ></v-text-field>
     </v-card-text>
     <div class="mx-4">
       <v-tooltip bottom>
         <template #activator="{ on, attrs }">
           <v-text-field
-            v-model="configName"
+            v-if="completeConfig"
+            v-model="saveAs"
+            :disabled="!messageType"
             label="Save As"
             v-bind="attrs"
             v-on="on"
@@ -154,3 +257,9 @@ export default {
     </div>
   </v-card>
 </template>
+
+<style scoped>
+.no-top {
+  margin-top: 1px;
+}
+</style>
