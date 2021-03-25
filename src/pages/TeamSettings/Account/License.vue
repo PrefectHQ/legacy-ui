7dd79bae985a06bd2652d4e6922a7c10d90a44ba
nicholas <znicholasbrown@gmail.com>
2021-03-24 20:20:52 -0400

    Add cancellation button with dialog (scaffolded)

--- src/pages/TeamSettings/Account/License.vue
+++ src/pages/TeamSettings/Account/License.vue
@@ -2,13 +2,17 @@
 import { mapGetters } from 'vuex'
 import { featureTypes } from '@/utils/features'
 import { PLANS_2021 } from '@/utils/plans'
+
+import ConfirmDialog from '@/components/ConfirmDialog'
 import ExternalLink from '@/components/ExternalLink'
 
 export default {
-  components: { ExternalLink },
+  components: { ConfirmDialog, ExternalLink },
   data() {
     return {
       allFeatures: featureTypes,
+      cancelPlanDialog: false,
+      cancelPlanloading: false,
       loading: false,
       plans: Object.values(PLANS_2021)
     }
@@ -123,165 +127,199 @@ export default {
       const feature = featureTypes.find(f => f.type == type)
       if (feature) return feature.link
       return ''
+    },
+    cancelPlan() {
+      // Call the downgrade mutation here, then reload the page
+      // console.log('cancelling')
     }
   }
 }
 </script>
 
 <template>
-  <v-card tile data-cy="license-card" :loading="loading">
-    <v-card-title class="mb-2 text-h4 font-weight-light">
-      Your plan
-      <v-spacer />
+  <div>
+    <v-card tile data-cy="license-card" :loading="loading">
+      <v-card-title class="mb-2 text-h4 font-weight-light">
+        Your plan
+        <v-spacer />
 
-      <v-btn
-        v-if="isSelfServe"
-        color="accentPink"
-        depressed
-        dark
-        small
-        :to="'/plans'"
-      >
-        Upgrade
-      </v-btn>
-    </v-card-title>
-
-    <v-card-subtitle class="pb-0">
-      You're on
-      {{ !isLegacy ? 'the' : 'a' }}
-      <v-icon :color="planColor" class="mr-1 pb-1" x-small>
-        cloud
-      </v-icon>
-      <span :class="`${planColor}--text`" class="text-capitalize"
-        >{{ planName
-        }}<span v-if="isLegacy" class="text-none"> (legacy)</span></span
-      >
-      plan<span v-if="isLegacy && isSelfServe"
-        >. <router-link :to="'/plans'">Upgrade now</router-link> to get access
-        to usage-based pricing and new features!
-      </span>
-      <br />
-    </v-card-subtitle>
-    <v-card-text>
-      <div class="d-flex flex-wrap mt-4">
-        <div
-          class="d-flex justify-start align-start py-4 px-8 my-2"
-          style="width: 50%;"
+        <v-btn
+          v-if="isSelfServe"
+          class="mr-1 blue-grey--text"
+          text
+          small
+          @click="cancelPlanDialog = true"
         >
-          <div class="mr-4">
-            <i class="primary--text fad fa-clouds fa-fw fa-3x" />
-          </div>
-          <div>
-            <div
-              class="text-h6 font-weight-regular text-capitalize utilGrayMid--text"
-            >
-              {{ planName
-              }}<span class="text-none">
-                {{ isLegacy ? '(legacy)' : '' }} plan
-              </span>
+          Cancel plan
+        </v-btn>
+
+        <v-btn
+          v-if="isSelfServe"
+          color="accentPink"
+          depressed
+          dark
+          small
+          :to="'/plans'"
+        >
+          Upgrade
+        </v-btn>
+      </v-card-title>
+
+      <v-card-subtitle class="pb-0">
+        You're on
+        {{ !isLegacy ? 'the' : 'a' }}
+        <v-icon :color="planColor" class="mr-1 pb-1" x-small>
+          cloud
+        </v-icon>
+        <span :class="`${planColor}--text`" class="text-capitalize"
+          >{{ planName
+          }}<span v-if="isLegacy" class="text-none"> (legacy)</span></span
+        >
+        plan<span v-if="isLegacy && isSelfServe"
+          >. <router-link :to="'/plans'">Upgrade now</router-link> to get access
+          to usage-based pricing and new features!
+        </span>
+        <br />
+      </v-card-subtitle>
+      <v-card-text>
+        <div class="d-flex flex-wrap mt-4">
+          <div
+            class="d-flex justify-start align-start py-4 px-8 my-2"
+            style="width: 50%;"
+          >
+            <div class="mr-4">
+              <i class="primary--text fad fa-clouds fa-fw fa-3x" />
             </div>
-            <div class="text-body-1">
-              Visit the
-              <ExternalLink href="https://prefect.io/pricing" target="_blank"
-                >pricing page</ExternalLink
+            <div>
+              <div
+                class="text-h6 font-weight-regular text-capitalize utilGrayMid--text"
               >
-              for more details
+                {{ planName
+                }}<span class="text-none">
+                  {{ isLegacy ? '(legacy)' : '' }} plan
+                </span>
+              </div>
+              <div class="text-body-1">
+                Visit the
+                <ExternalLink href="https://prefect.io/pricing" target="_blank"
+                  >pricing page</ExternalLink
+                >
+                for more details
+              </div>
             </div>
           </div>
-        </div>
 
-        <div
-          v-if="
-            planType === 'FREE_2021' ||
-              planType === 'STARTER_2021' ||
-              planType === 'STANDARD_2021'
-          "
-          class="d-flex justify-start align-start py-4 px-8 my-2"
-          style="width: 50%;"
-        >
-          <div class="mr-4">
-            <i class="primary--text fad fa-tasks fa-fw fa-3x" />
+          <div
+            v-if="
+              planType === 'FREE_2021' ||
+                planType === 'STARTER_2021' ||
+                planType === 'STANDARD_2021'
+            "
+            class="d-flex justify-start align-start py-4 px-8 my-2"
+            style="width: 50%;"
+          >
+            <div class="mr-4">
+              <i class="primary--text fad fa-tasks fa-fw fa-3x" />
+            </div>
+            <div>
+              <div class="text-h6 font-weight-regular utilGrayMid--text">
+                10,000 free runs / month
+              </div>
+              <div v-if="planType !== 'FREE_2021'" class="text-body-1">
+                Your first 10,000 successful runs per month are on us!
+              </div>
+              <div v-else class="text-body-1">
+                10,000 successful runs each month!
+              </div>
+            </div>
           </div>
-          <div>
-            <div class="text-h6 font-weight-regular utilGrayMid--text">
-              10,000 free runs / month
-            </div>
-            <div v-if="planType !== 'FREE_2021'" class="text-body-1">
-              Your first 10,000 successful runs per month are on us!
-            </div>
-            <div v-else class="text-body-1">
-              10,000 successful runs each month!
-            </div>
-          </div>
-        </div>
 
-        <div
-          class="d-flex justify-start align-start py-4 px-8 my-2"
-          style="width: 50%;"
-        >
-          <div class="mr-4">
-            <i
-              :class="
-                `primary--text fad fa-user${userNum > 1 ? 's' : ''} fa-fw fa-3x`
-              "
-            />
+          <div
+            class="d-flex justify-start align-start py-4 px-8 my-2"
+            style="width: 50%;"
+          >
+            <div class="mr-4">
+              <i
+                :class="
+                  `primary--text fad fa-user${
+                    userNum > 1 ? 's' : ''
+                  } fa-fw fa-3x`
+                "
+              />
+            </div>
+            <div>
+              <div class="text-h6 font-weight-regular utilGrayMid--text">
+                {{ userNum ? userNum : 'Unlimited' }} {{ userOrUsers }}
+              </div>
+              <div v-if="userNum" class="text-body-1">
+                You can invite up to {{ userNum }} {{ userOrUsers }}
+              </div>
+              <div v-else class="text-body-1">
+                You have unlimited users!
+              </div>
+            </div>
           </div>
-          <div>
-            <div class="text-h6 font-weight-regular utilGrayMid--text">
-              {{ userNum ? userNum : 'Unlimited' }} {{ userOrUsers }}
-            </div>
-            <div v-if="userNum" class="text-body-1">
-              You can invite up to {{ userNum }} {{ userOrUsers }}
-            </div>
-            <div v-else class="text-body-1">
-              You have unlimited users!
-            </div>
-          </div>
-        </div>
 
-        <div
-          v-if="flowConcurrency"
-          class="d-flex justify-start align-start py-4 px-8 my-2"
-          style="width: 50%;"
-        >
-          <div class="mr-4">
-            <i class="primary--text pi-flow pi-3x pi-fw" />
-          </div>
-          <div>
-            <div class="text-h6 font-weight-regular utilGrayMid--text">
-              {{ `${flowConcurrency} concurrent ${flowOrFlows}` }}
+          <div
+            v-if="flowConcurrency"
+            class="d-flex justify-start align-start py-4 px-8 my-2"
+            style="width: 50%;"
+          >
+            <div class="mr-4">
+              <i class="primary--text pi-flow pi-3x pi-fw" />
             </div>
-            <div class="text-body-1">
-              You can have {{ flowConcurrency }} {{ flowOrFlows }} running at a
-              time
+            <div>
+              <div class="text-h6 font-weight-regular utilGrayMid--text">
+                {{ `${flowConcurrency} concurrent ${flowOrFlows}` }}
+              </div>
+              <div class="text-body-1">
+                You can have {{ flowConcurrency }} {{ flowOrFlows }} running at
+                a time
+              </div>
             </div>
           </div>
-        </div>
 
-        <div
-          class="d-flex justify-start align-start py-4 px-8 my-2"
-          style="width: 50%;"
-        >
-          <div class="mr-4">
-            <i class="primary--text fad fa-history fa-fw fa-3x" />
-          </div>
-          <div>
-            <div class="text-h6 font-weight-regular utilGrayMid--text">
-              {{
-                historyRetention ? historyRetention + ' days of ' : 'Unlimited'
-              }}
-              history
+          <div
+            class="d-flex justify-start align-start py-4 px-8 my-2"
+            style="width: 50%;"
+          >
+            <div class="mr-4">
+              <i class="primary--text fad fa-history fa-fw fa-3x" />
             </div>
-            <div v-if="historyRetention" class="text-body-1">
-              Flow and task run history is retained for
-              {{ historyRetention }} days
-            </div>
-            <div v-else class="text-body-1">
-              You can view run history forever!
+            <div>
+              <div class="text-h6 font-weight-regular utilGrayMid--text">
+                {{
+                  historyRetention
+                    ? historyRetention + ' days of '
+                    : 'Unlimited'
+                }}
+                history
+              </div>
+              <div v-if="historyRetention" class="text-body-1">
+                Flow and task run history is retained for
+                {{ historyRetention }} days
+              </div>
+              <div v-else class="text-body-1">
+                You can view run history forever!
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </v-card-text>
-  </v-card>
+      </v-card-text>
+    </v-card>
+
+    <ConfirmDialog
+      v-model="cancelPlanDialog"
+      type="error"
+      :dialog-props="{ 'max-width': '500' }"
+      :disabled="cancelPlanloading"
+      :loading="cancelPlanloading"
+      title="Are you sure?"
+      @confirm="cancelPlan"
+    >
+      This will cancel your current plan and put you on the Free plan. Any
+      outstanding balance will be billed immediately. Also Jeremiah Lowin will
+      cry. 😢
+    </ConfirmDialog>
+  </div>
 </template>
