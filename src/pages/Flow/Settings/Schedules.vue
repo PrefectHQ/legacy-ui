10bd7dd74020f0d0d3da21d4ff4cbb3ff88caf2f
Natalie Smith <thatgalnatalie@gmail.com>
2021-04-15 10:17:59 -0700

    modify check

--- src/pages/Flow/Settings/Schedules.vue
+++ src/pages/Flow/Settings/Schedules.vue
@@ -426,9 +426,7 @@ export default {
               <div v-show="selectedTab === 1">
                 <div
                   v-if="
-                    defaultParameters.length === 0 &&
-                      clock.parameter_defaults &&
-                      Object.keys(clock.parameter_defaults).length === 0
+                    defaultParameters.length === 0 && !clock.parameter_defaults
                   "
                   class="mt-8 text-body-1"
                 >
