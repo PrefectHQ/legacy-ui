1833ebb7f846a12ec41658b9dfd5945e85516163
Natalie Smith <thatgalnatalie@gmail.com>
2021-04-30 14:34:50 -0700

    add json editor

--- src/pages/TeamSettings/KV.vue
+++ src/pages/TeamSettings/KV.vue
@@ -1,253 +1,3 @@
-<!--
-<script>
-import JsonInput from '@/components/CustomInputs/JsonInput'
-import ManagementLayout from '@/layouts/ManagementLayout'
-import ConfirmDialog from '@/components/ConfirmDialog'
-import Alert from '@/components/Alert'
-
-export default {
-  components: {
-    JsonInput,
-    ManagementLayout,
-    ConfirmDialog,
-    Alert
-  },
-  data() {
-    return {
-      items: [
-        {
-          id: 1,
-          key: 'prod.config.ram',
-          value: '8GB'
-        },
-        {
-          id: 2,
-          key: 'foo',
-          value: 'bar'
-        },
-        {
-          id: 3,
-          key: 'dev.config.cpu',
-          value: '{a: 1, b: 2}'
-        }
-      ],
-      expandAll: false,
-      search: null,
-      json: false,
-      jsonInput: '{}',
-      secretModifyDialog: false,
-      secretDeleteDialog: false
-    }
-  },
-  mounted() {
-    this.jsonInput = JSON.stringify(this.items)
-  },
-  computed: {
-    queryData() {
-      // return this.items.map(item => {
-      //   return item.key.split('.').reduceRight(
-      //     (o, x) => ({
-      // name: [x][0],
-      // value: item.value,
-      // children: Object.keys(o).length === 0 ? [] : [o]
-      //     }),
-      //     {}
-      //   )
-      // })
-
-      //NOTE: this will return the value
-      return this.items.map(item => {
-        return item.key.split('.').reduceRight(
-          (o, x) => ({
-            name: [x][0],
-            children: Object.keys(o).length === 0 ? [{ name: item.value }] : [o]
-          }),
-          {}
-        )
-      })
-
-      // return []
-    },
-    keyNames() {
-      return this.items.map(item => item.key.split('.')[0])
-    }
-  },
-  methods: {
-    handleEdit(item) {
-      console.log(item)
-      this.secretModifyDialog = true
-    },
-    handleDelete(item) {
-      console.log(item)
-      this.secretDeleteDialog = true
-    },
-    addKey() {
-      this.secretModifyDialog = true
-    },
-    filter(array, text) {
-      const getNodes = (result, object) => {
-        if (object.name === text) {
-          result.push(object)
-          return result
-        }
-        if (Array.isArray(object.children)) {
-          const nodes = object.children.reduce(getNodes, [])
-          if (nodes.length) result.push({ ...object, nodes })
-        }
-        return result
-      }
-
-      return array.reduce(getNodes, []).length > 0
-        ? array.reduce(getNodes, [])
-        : this.queryData
-    }
-  }
-}
-</script>
-
-<template>
-  <div>
-    <ManagementLayout :show="true" control-show>
-      <template #title>Key/Value</template>
-
-      <template #subtitle>
-        Manage your key/value
-        <sup>
-          <v-icon x-small>
-            open_in_new
-          </v-icon>
-        </sup>
-      </template>
-
-      <template #cta>
-        <v-btn color="primary" class="white--text" large @click="addKey">
-          <v-icon left>
-            add
-          </v-icon>
-          Add Key
-        </v-btn>
-      </template>
-    </ManagementLayout>
-    <v-card elevation="2" tile class="mx-auto mt-10">
-      <v-sheet class="pa-4">
-        <v-text-field
-          v-model="search"
-          placeholder="Search for a key"
-          flat
-          outlined
-          hide-details
-          clearable
-          prepend-inner-icon="search"
-          clear-icon="close"
-        ></v-text-field>
-        <div
-          class="d-flex justify-end align-end mb-1 mt-4"
-          style="width: 100%;"
-        >
-          <v-btn
-            x-small
-            class="text-normal mr-2"
-            depressed
-            color="utilGrayLight"
-          >
-            <v-icon small>expand_more</v-icon>
-            Expand All
-          </v-btn>
-
-          <v-switch
-            v-model="json"
-            inset
-            label="JSON"
-            class="mt-0 small-switch v-input--reverse"
-            hide-details
-          ></v-switch>
-        </div>
-
-        <div v-if="json">
-          <JsonInput
-            ref="json-input"
-            v-model="jsonInput"
-            prepend-icon="fad fa-file-code"
-            prepend-icon-label="JSON"
-            selected-type="json"
-          />
-        </div>
-      </v-sheet>
-      <v-card-text class="pa-0">
-        <v-treeview
-          ref="tree"
-          :items="filter(queryData, search)"
-          open-on-click
-          hoverable
-          expand-icon="expand_more"
-        >
-          <template v-slot:append="{ item }">
-            <v-icon
-              v-if="keyNames.includes(item.name)"
-              text
-              fab
-              small
-              color="primary"
-              @click="handleEdit(item)"
-              >edit</v-icon
-            >
-            <v-icon
-              v-if="keyNames.includes(item.name)"
-              text
-              fab
-              small
-              color="error"
-              @click="handleDelete(item)"
-              >delete</v-icon
-            >
-          </template>
-        </v-treeview>
-      </v-card-text>
-    </v-card>
-
-    <ConfirmDialog
-      v-model="secretModifyDialog"
-      :dialog-props="{ 'max-width': '75vh' }"
-      title="Create New Key"
-    >
-      <v-text-field
-        class="_lr-hide mt-6"
-        data-private
-        single-line
-        outlined
-        dense
-        placeholder="Key Name"
-        prepend-inner-icon="vpn_key"
-        validate-on-blur
-      />
-
-      <JsonInput
-        ref="secretRef"
-        class="text-body-1"
-        placeholder-text="Enter your value"
-      >
-      </JsonInput>
-    </ConfirmDialog>
-
-    <ConfirmDialog
-      v-model="secretDeleteDialog"
-      type="error"
-      confirm-text="Delete"
-      :dialog-props="{ 'max-width': '500' }"
-      title="Are you sure you want to delete this key?"
-    >
-      This action cannot be undone.
-    </ConfirmDialog>
-
-    <Alert
-      type="error"
-      message="message"
-      :offset-x="$vuetify.breakpoint.mdAndUp ? 256 : 56"
-    ></Alert>
-  </div>
-</template>
--->
-
 <script>
 import JsonInput from '@/components/CustomInputs/JsonInput'
 import ManagementLayout from '@/layouts/ManagementLayout'
@@ -371,7 +121,6 @@ export default {
       </template>
     </ManagementLayout>
     <v-card tile class="mx-auto">
-      <!-- SEARCH -->
       <v-card-title>
         <v-text-field
           v-model="search"
@@ -382,29 +131,26 @@ export default {
           prepend-inner-icon="search"
           clear-icon="close"
         ></v-text-field>
-
-        <div
-          class="d-flex justify-end align-end mb-1 mt-4"
-          style="width: 100%;"
-        >
-          <v-switch
-            v-model="json"
-            inset
-            label="JSON"
-            class="mt-0 small-switch v-input--reverse"
-            hide-details
-          ></v-switch>
-        </div>
-
-        <div v-if="json">
-          <JsonInput
-            ref="json-input"
-            prepend-icon="fad fa-file-code"
-            prepend-icon-label="JSON"
-            selected-type="json"
-          />
-        </div>
       </v-card-title>
+
+      <div class="d-flex justify-end align-end mb-1 mt-4" style="width: 100%;">
+        <v-switch
+          v-model="json"
+          inset
+          label="JSON"
+          class="mt-0 small-switch v-input--reverse"
+          hide-details
+        ></v-switch>
+      </div>
+
+      <div v-if="json" style="padding: 10px;">
+        <JsonInput
+          ref="json-input"
+          prepend-icon="fad fa-file-code"
+          prepend-icon-label="JSON"
+          selected-type="json"
+        />
+      </div>
 
       <v-card-text class="pa-0">
         <!-- TABLE -->
