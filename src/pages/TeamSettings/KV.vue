d389755697dab1e88a303eceef56277a5ce179e3
Natalie Smith <thatgalnatalie@gmail.com>
2021-04-29 18:14:22 -0700

    rm namespacing/treeview, add in table

--- src/pages/TeamSettings/KV.vue
+++ src/pages/TeamSettings/KV.vue
@@ -1,14 +1,5 @@
+<!--
 <script>
-// "foo-moo.bar.baz".split(".") => [ 'foo-moo', 'bar', 'baz' ]
-// "foo.bar.baz".split(".") => [ 'foo', 'bar', 'baz' ]
-
-/*
-[{
-"dev.config.ram": "8 gb",
-"regular": "foo"
-}]
-*/
-
 import JsonInput from '@/components/CustomInputs/JsonInput'
 import ManagementLayout from '@/layouts/ManagementLayout'
 import ConfirmDialog from '@/components/ConfirmDialog'
@@ -31,26 +22,25 @@ export default {
         },
         {
           id: 2,
-          key: 'beep',
-          value: 'boop'
+          key: 'foo',
+          value: 'bar'
         },
         {
           id: 3,
           key: 'dev.config.cpu',
           value: '{a: 1, b: 2}'
-        },
-        {
-          id: 4,
-          key: 'prod.config.ram2',
-          value: '9GB'
         }
       ],
       expandAll: false,
       search: null,
       json: false,
+      jsonInput: '{}',
       secretModifyDialog: false,
       secretDeleteDialog: false
     }
+  },
+  mounted() {
+    this.jsonInput = JSON.stringify(this.items)
   },
   computed: {
     queryData() {
@@ -84,11 +74,11 @@ export default {
   },
   methods: {
     handleEdit(item) {
-      //console.log(item)
+      console.log(item)
       this.secretModifyDialog = true
     },
     handleDelete(item) {
-      //console.log(item)
+      console.log(item)
       this.secretDeleteDialog = true
     },
     addKey() {
@@ -236,30 +226,6 @@ export default {
         class="text-body-1"
         placeholder-text="Enter your value"
       >
-        <!-- <v-menu top offset-y>
-          <template #activator="{ on }">
-            <v-btn
-              text
-              small
-              class="position-absolute"
-              :style="{
-                bottom: '25px',
-                right: '80px',
-                'z-index': 3
-              }"
-              color="accent"
-              v-on="on"
-              >Type</v-btn
-            >
-          </template>
-          <v-list>
-            <v-list-item-group v-model="selectedTypeIndex" color="primary">
-              <v-list-item v-for="(item, index) in secretTypes" :key="index">
-                <v-list-item-title>{{ item.text }} </v-list-item-title>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-menu> -->
       </JsonInput>
     </ConfirmDialog>
 
@@ -280,3 +246,227 @@ export default {
     ></Alert>
   </div>
 </template>
+-->
+
+<script>
+import JsonInput from '@/components/CustomInputs/JsonInput'
+import ManagementLayout from '@/layouts/ManagementLayout'
+import ConfirmDialog from '@/components/ConfirmDialog'
+export default {
+  components: {
+    JsonInput,
+    ManagementLayout,
+    ConfirmDialog
+  },
+  data() {
+    return {
+      search: '',
+      headers: [
+        { text: 'Key', value: 'key' },
+        { text: 'Value', value: 'value' },
+        { text: 'Created By', value: 'created' },
+        { text: 'Updated', value: 'updated' },
+        {
+          text: '',
+          value: 'actions',
+          align: 'right',
+          sortable: false
+        }
+      ],
+      items: [
+        {
+          key: 'prod.config.ram',
+          value: '8gb',
+          created: '2021-04-29T23:34:12.488Z',
+          updated: '2021-04-29T23:36:48.494Z'
+        },
+        {
+          key: 'prod.config.ram2',
+          value: '16gb',
+          created: '2021-04-29T23:34:12.488Z',
+          updated: '2021-04-29T23:36:48.494Z'
+        },
+        {
+          key: 'dev.config',
+          value: JSON.stringify([
+            {
+              _id: '608b43a3e8451372b62f0ebe',
+              index: 0,
+              guid: '8fddc0d3-47f9-4335-a543-849341986864',
+              isActive: false,
+              balance: '$2,441.60',
+              picture: 'http://placehold.it/32x32',
+              age: 33,
+              eyeColor: 'blue',
+              name: 'Robles Dawson',
+              gender: 'male',
+              company: 'MANUFACT',
+              email: 'roblesdawson@manufact.com',
+              phone: '+1 (900) 520-2622',
+              address: '991 Osborn Street, Tyro, District Of Columbia, 1328',
+              about:
+                'Cillum sunt adipisicing eiusmod minim qui fugiat. Reprehenderit mollit cupidatat occaecat est. Velit nulla nostrud id incididunt dolor in magna nisi commodo consequat voluptate. Non adipisicing ea minim cupidatat sint ipsum. Commodo in ipsum fugiat non laborum dolore dolor pariatur cillum laborum. Aute ex elit ex culpa Lorem in do cillum sit. Deserunt eu consectetur fugiat enim et mollit.\r\n',
+              registered: '2018-08-10T07:57:29 +07:00',
+              latitude: 24.154272,
+              longitude: -160.265207,
+              tags: ['aliquip', 'anim', 'eu', 'ex', 'qui', 'officia', 'irure'],
+              friends: [
+                {
+                  id: 0,
+                  name: 'Wright Rhodes'
+                },
+                {
+                  id: 1,
+                  name: 'Nadine Payne'
+                },
+                {
+                  id: 2,
+                  name: 'Solis Mccullough'
+                }
+              ],
+              greeting: 'Hello, Robles Dawson! You have 1 unread messages.',
+              favoriteFruit: 'banana'
+            }
+          ]),
+          created: '2021-01-17T11:24:00.000Z',
+          updated: '2021-01-18T11:24:00.000Z'
+        },
+        {
+          key: 'foo',
+          value: 'bar',
+          created: '2021-04-17T10:24:00.000Z',
+          updated: '2021-04-17T10:24:00.000Z'
+        }
+      ],
+      json: false
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <ManagementLayout :show="true" control-show>
+      <template #title>Key/Value</template>
+
+      <template #subtitle>
+        Manage your key/value
+        <sup>
+          <v-icon x-small>
+            open_in_new
+          </v-icon>
+        </sup>
+      </template>
+
+      <template #cta>
+        <v-btn color="primary" class="white--text" large @click="addKey">
+          <v-icon left>
+            add
+          </v-icon>
+          Add Key
+        </v-btn>
+      </template>
+    </ManagementLayout>
+    <v-card tile class="mx-auto">
+      <!-- SEARCH -->
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          placeholder="Search for a key"
+          flat
+          hide-details
+          clearable
+          prepend-inner-icon="search"
+          clear-icon="close"
+        ></v-text-field>
+
+        <div
+          class="d-flex justify-end align-end mb-1 mt-4"
+          style="width: 100%;"
+        >
+          <v-switch
+            v-model="json"
+            inset
+            label="JSON"
+            class="mt-0 small-switch v-input--reverse"
+            hide-details
+          ></v-switch>
+        </div>
+
+        <div v-if="json">
+          <JsonInput
+            ref="json-input"
+            prepend-icon="fad fa-file-code"
+            prepend-icon-label="JSON"
+            selected-type="json"
+          />
+        </div>
+      </v-card-title>
+
+      <v-card-text class="pa-0">
+        <!-- TABLE -->
+        <v-data-table :headers="headers" :items="items" :search="search">
+          <!-- ACTIONS -->
+          <template #item.actions>
+            <v-tooltip bottom>
+              <template #activator="{ on }">
+                <v-btn text fab x-small color="primary" v-on="on">
+                  <v-icon>edit</v-icon>
+                </v-btn>
+              </template>
+              Modify key
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template #activator="{ on }">
+                <v-btn text fab x-small color="error" v-on="on">
+                  <v-icon>delete</v-icon>
+                </v-btn>
+              </template>
+              Delete key
+            </v-tooltip>
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
+
+    <ConfirmDialog
+      :dialog-props="{ 'max-width': '75vh' }"
+      title="Create New Key"
+    >
+      <v-text-field
+        class="_lr-hide mt-6"
+        data-private
+        single-line
+        outlined
+        dense
+        placeholder="Key Name"
+        prepend-inner-icon="vpn_key"
+        validate-on-blur
+      />
+
+      <JsonInput
+        ref="secretRef"
+        class="text-body-1"
+        placeholder-text="Enter your value"
+      >
+      </JsonInput>
+    </ConfirmDialog>
+
+    <ConfirmDialog
+      type="error"
+      confirm-text="Delete"
+      :dialog-props="{ 'max-width': '500' }"
+      title="Are you sure you want to delete this key?"
+    >
+      This action cannot be undone.
+    </ConfirmDialog>
+
+    <Alert
+      type="error"
+      message="message"
+      :offset-x="$vuetify.breakpoint.mdAndUp ? 256 : 56"
+    ></Alert>
+  </div>
+</template>
+
+<style></style>
