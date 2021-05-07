f25d2bb2535ae8a80ce0ab0e47a07c827e2cca9b
Natalie Smith <thatgalnatalie@gmail.com>
2021-05-07 00:43:08 -0700

    make the add, edit, and delete methods work

--- src/pages/TeamSettings/KV.vue
+++ src/pages/TeamSettings/KV.vue
@@ -2,12 +2,15 @@
 import JsonInput from '@/components/CustomInputs/JsonInput'
 import ManagementLayout from '@/layouts/ManagementLayout'
 import ConfirmDialog from '@/components/ConfirmDialog'
+import Alert from '@/components/Alert'
 import MenuTooltip from '@/components/MenuTooltip'
 import { formatTime } from '@/mixins/formatTimeMixin'
+import { mapGetters } from 'vuex'
 
 export default {
   components: {
     JsonInput,
+    Alert,
     ManagementLayout,
     ConfirmDialog,
     MenuTooltip
@@ -15,8 +18,32 @@ export default {
   mixins: [formatTime],
   data() {
     return {
-      jsonInput: '{}',
-      keyInput: '',
+      isFetchingKV: true,
+      isDeletingKV: false,
+      // Store previous kv name when modifying kv
+      // This is used to delete the kv with that name and create a new kv with a separate value
+      previousKVName: null,
+      selectedKV: '',
+      // Alert data
+      alertShow: false,
+      alertMessage: '',
+      alertType: null,
+      errorMessage: '',
+      rules: {
+        required: val => !!val || 'This field is required.'
+      },
+      KvValueInput: '',
+      isKvUpdate: false,
+      selectedTypeIndex: 0,
+      kvTypes: [
+        { value: 'auto', text: 'Auto' },
+        { value: 'string', text: 'String' },
+        { value: 'json', text: 'JSON' }
+      ],
+      invalidKV: false,
+      isSettingKV: false,
+      jsonInput: '',
+      keyInput: null,
       search: '',
       expanded: [],
       headers: [
@@ -105,30 +132,196 @@ export default {
       keyDeleteDialog: false
     }
   },
-  mounted() {
-    // this.jsonInput = JSON.stringify(this.items)
-    // this.jsonInput = JSON.parse(this.items)
+  computed: {
+    ...mapGetters('tenant', ['tenant']),
+    disableConfirm() {
+      if (!this.keyInput) return false
+      if (!this.KvValueInput) return false
+      if (this.invalidKV) return false
+      return true
+    }
   },
   methods: {
-    convertJSON(item) {
-      const dict = {}
-      dict[item.key] = item.value
-      return dict
+    openKVModifyDialog(item) {
+      this.selectedKV = item
+      this.isKvUpdate = true
+      this.previousKVName = item.key
+      this.keyInput = item.key
+      this.KvValueInput = item.value
+      this.keyModifyDialog = true
     },
-    setKey() {
-      // set loader
-      // validate key and json
-      // possible mutation
-      // const kvResult = await this.$apollo.mutate({
-      //   mutation: require('@/graphql/KV/set-KV.gql'),
+    openKVDeleteDialog(item) {
+      this.selectedKV = item
+      this.keyDeleteDialog = true
+    },
+    handleAlert(type, message) {
+      this.alertType = type
+      this.alertMessage = message
+      this.alertShow = true
+    },
+    resetSelectedKV() {
+      this.selectedKV = null
+      this.keyInput = null
+      this.KvValueInput = null
+      this.selectedTypeIndex = 0
+      this.invalidKV = false
+    },
+    setKV() {
+      this.isSettingKV = true
+
+      if (this.selectedTypeIndex === 2 && !this.validKVJSON()) {
+        this.isSettingKV = false
+        this.invalidKV = true
+        return
+      }
+      if (this.isKvUpdate) {
+        // modifying
+        this.deleteKV({ name: this.previousKVName }, { isModifying: true })
+      }
+
+      let value = this.KvValueInput
+      if (this.selectedTypeIndex === 0) {
+        try {
+          value = JSON.parse(this.KvValueInput)
+        } catch {
+          try {
+            value = String.raw`${this.KvValueInput}`
+          } catch {
+            value = JSON.stringify(this.KvValueInput)
+          }
+        }
+      }
+      if (this.selectedTypeIndex === 2) value = JSON.parse(this.KvValueInput)
+
+      // call muatation
+      const fakeResult = new Promise(resolve => {
+        setTimeout(() => {
+          resolve({
+            data: {
+              set_kv: {
+                success: true,
+                result: {
+                  id: Math.floor(Math.random() * 100 + 1),
+                  key: this.keyInput,
+                  value: value || [],
+                  created: this.tenant.name,
+                  updated: new Date(
+                    +new Date() - Math.floor(Math.random() * 10000000000)
+                  )
+                }
+              }
+            }
+          })
+        }, 2000)
+      })
+      fakeResult.then(res => {
+        if (res?.data?.set_kv?.success) {
+          // this.$apollo.queries.kv.refetch()
+          this.keyModifyDialog = false
+          this.items.push(res?.data?.set_kv?.result)
+          this.resetSelectedKV()
+          this.handleAlert(
+            'success',
+            `KV ${this.isKvUpdate ? 'updated' : 'added'}.`
+          )
+        } else {
+          this.handleAlert(
+            'error',
+            `Something went wrong when ${
+              this.isKvUpdate ? 'updating' : 'creating'
+            } this KV. Please try again. If this error persists, please contact help@prefect.io.`
+          )
+        }
+      })
+
+      // this.isSettingKV = false
+      // this.keyInput = null
+      // this.KvValueInput = null
+    },
+
+    deleteKV(kv, opts = {}) {
+      this.isDeletingKV = true
+      //  mutation
+      // const KvResult = await this.$apollo.mutate({
+      //   mutation: require('@/graphql/KV/delete-kv.gql'),
       //   variables: {
-      //     key: this.keyInput,
-      //     value: this.jsonInput || []
+      //     name: kv.name
       //   }
       // })
-      // check if the mutation was successful or not and display the accroding message
+
+      const fakeResult = new Promise(resolve => {
+        setTimeout(() => {
+          resolve({
+            data: {
+              delete_kv: {
+                success: true,
+                result: kv
+              }
+            }
+          })
+        }, 2000)
+      })
+
+      fakeResult.then(res => {
+        if (res?.data?.delete_kv?.success) {
+          if (!opts.isModifying) {
+            // this.$apollo.queries.kv.refetch()
+            this.keyDeleteDialog = false
+            this.handleAlert('success', 'KV deleted.')
+          }
+        } else {
+          this.handleAlert(
+            'error',
+            'Something went wrong while trying to delete this kv. Please try again. If this error persists, reach out to help@prefect.io.'
+          )
+        }
+      })
+
+      // this.isDeletingKV = false
+    },
+
+    validKVJSON() {
+      this.invalidKV = false
+      if (this.selectedTypeIndex !== 2) {
+        this.$refs.kvRef.removeJsonErrors()
+        return true
+      }
+      if (!this.$refs.kvRef) {
+        this.$refs.kvRef.removeJsonErrors()
+        return true
+      }
+      // Check JSON using the JsonInput component's validation (need to check for true over truthy here because of the way the jsonInput returns for other components)
+      if (this.$refs.kvRef.validateJson() === true) return true
+      return false
+    },
+    setInvalidKV(event) {
+      this.invalidKV = event
     }
   }
+  //  apollo: {
+  //   kv: {
+  //     query: require('@/graphql/Tenant/get-kv.gql'),
+  //     result() {
+  //       this.isFetchingKV = false
+  //     },
+  //     update(data) {
+  //       return data?.kv?.map(item => ({ key: item.key, value: null }))
+  //     },
+  //     // skip() {
+  //     //   return this.isReadOnlyUser
+  //     // },
+  //     error() {
+  //       this.isFetchingKV = false
+  //       if (!this.isReadOnlyUser) {
+  //         this.handleAlert(
+  //           'error',
+  //           'Something went wrong while trying to fetch kv. Please try again. If this error persists, please contact help@prefect.io.'
+  //         )
+  //       }
+  //     },
+  //     fetchPolicy: 'network-only'
+  //   }
+  // }
 }
 </script>
 
@@ -205,13 +398,60 @@ export default {
           <!-- ACTIONS -->
           <template v-slot:expanded-item="{ headers, item }">
             <td :colspan="headers.length">
+              <div
+                class="d-flex justify-end align-end mt-5"
+                style="width: 100%;"
+              >
+                <v-btn
+                  x-small
+                  class="text-normal mr-2"
+                  depressed
+                  color="utilGrayLight"
+                  title="Reset"
+                >
+                  Reset
+                  <v-icon small>refresh</v-icon>
+                </v-btn>
+              </div>
               <JsonInput
-                :value="JSON.stringify(convertJSON(item))"
-                ref="json-input"
-                prepend-icon="fad fa-file-code"
-                prepend-icon-label="JSON"
-                selected-type="json"
-              />
+                :value="item.value"
+                ref="kvRef"
+                class="text-body-1 mt-2 mb-5"
+                :selected-type="kvTypes[selectedTypeIndex].value"
+                placeholder-text="Enter your value"
+                @invalid-secret="setInvalidKV"
+              >
+                <v-menu top offset-y>
+                  <template #activator="{ on }">
+                    <v-btn
+                      text
+                      small
+                      class="position-absolute"
+                      :style="{
+                        bottom: '25px',
+                        right: '80px',
+                        'z-index': 3
+                      }"
+                      color="accent"
+                      v-on="on"
+                      >Type</v-btn
+                    >
+                  </template>
+                  <v-list>
+                    <v-list-item-group
+                      v-model="selectedTypeIndex"
+                      color="primary"
+                    >
+                      <v-list-item
+                        v-for="(item, index) in kvTypes"
+                        :key="index"
+                      >
+                        <v-list-item-title>{{ item.text }} </v-list-item-title>
+                      </v-list-item>
+                    </v-list-item-group>
+                  </v-list>
+                </v-menu>
+              </JsonInput>
             </td>
           </template>
           <template #item.value="{item}">
@@ -233,7 +473,7 @@ export default {
           <template #item.updated="{item}">
             {{ formatTime(item.updated) }}
           </template>
-          <template #item.actions>
+          <template #item.actions="{item}">
             <v-tooltip bottom>
               <template #activator="{ on }">
                 <v-btn
@@ -242,7 +482,7 @@ export default {
                   x-small
                   color="primary"
                   v-on="on"
-                  @click="keyModifyDialog = true"
+                  @click="openKVModifyDialog(item)"
                 >
                   <v-icon>edit</v-icon>
                 </v-btn>
@@ -257,7 +497,7 @@ export default {
                   x-small
                   color="error"
                   v-on="on"
-                  @click="keyDeleteDialog = true"
+                  @click="openKVDeleteDialog(item)"
                 >
                   <v-icon>delete</v-icon>
                 </v-btn>
@@ -280,12 +520,15 @@ export default {
 
     <ConfirmDialog
       v-model="keyModifyDialog"
-      @confirm="setKey"
       :dialog-props="{ 'max-width': '75vh' }"
-      title="Create New Key"
+      :disabled="!disableConfirm"
+      :loading="isSettingKV"
+      :title="isKvUpdate ? 'Modify KV' : 'Create New KV'"
+      @confirm="setKV"
     >
       <v-text-field
         v-model="keyInput"
+        :rules="[rules.required]"
         class="_lr-hide mt-6"
         data-private
         single-line
@@ -297,27 +540,56 @@ export default {
       />
 
       <JsonInput
-        v-model="jsonInput"
-        ref="secretRef"
+        v-model="KvValueInput"
+        ref="kvRef"
         class="text-body-1"
+        :selected-type="kvTypes[selectedTypeIndex].value"
         placeholder-text="Enter your value"
+        @invalid-secret="setInvalidKV"
       >
+        <v-menu top offset-y>
+          <template #activator="{ on }">
+            <v-btn
+              text
+              small
+              class="position-absolute"
+              :style="{
+                bottom: '25px',
+                right: '80px',
+                'z-index': 3
+              }"
+              color="accent"
+              v-on="on"
+              >Type</v-btn
+            >
+          </template>
+          <v-list>
+            <v-list-item-group v-model="selectedTypeIndex" color="primary">
+              <v-list-item v-for="(item, index) in kvTypes" :key="index">
+                <v-list-item-title>{{ item.text }} </v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-menu>
       </JsonInput>
     </ConfirmDialog>
 
     <ConfirmDialog
       v-model="keyDeleteDialog"
       type="error"
+      :loading="isDeletingKV"
       confirm-text="Delete"
       :dialog-props="{ 'max-width': '500' }"
       title="Are you sure you want to delete this key?"
+      @confirm="deleteKV(selectedKV)"
     >
       This action cannot be undone.
     </ConfirmDialog>
 
     <Alert
-      type="error"
-      message="message"
+      v-model="alertShow"
+      :type="alertType"
+      :message="alertMessage"
       :offset-x="$vuetify.breakpoint.mdAndUp ? 256 : 56"
     ></Alert>
   </div>
