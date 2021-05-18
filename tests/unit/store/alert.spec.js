ab9265dccb41bd5a77c5d00a3cff8f98f2145685
nicholas <znicholasbrown@gmail.com>
2021-05-18 17:37:49 -0400

    Add notification getter tests

--- tests/unit/store/alert.spec.js
+++ tests/unit/store/alert.spec.js
@@ -6,60 +6,14 @@ jest.mock('@/main', () => {
   }
 })
 
+import Vuex from 'vuex'
+import alert from '@/store/alert'
 import store from '@/store'
 jest.useFakeTimers()
 
 describe('Alert Vuex Module', () => {
-  it('should initially have a show key that is false and other keys should be falsey', () => {
-    expect(store.getters['alert/getAlert']).toStrictEqual({
-      alertShow: false,
-      alertMessage: '',
-      alertType: null,
-      alertLink: null,
-      linkText: ''
-    })
-  })
-
-  describe('mutations', () => {
-    it('updates the state when setAl is called', () => {
-      expect(store.getters['alert/getAlert']).toStrictEqual({
-        alertShow: false,
-        alertMessage: '',
-        alertType: null,
-        alertLink: null,
-        linkText: ''
-      })
-      store.commit('alert/setAl', {
-        alertShow: true,
-        alertMessage: 'Testing testing 123',
-        alertType: 'error',
-        alertLink: null,
-        linkText: ''
-      })
-      expect(store.getters['alert/getAlert']).toStrictEqual({
-        alertShow: true,
-        alertMessage: 'Testing testing 123',
-        alertType: 'error',
-        alertLink: null,
-        linkText: ''
-      })
-    })
-    it('empties the state when setEmpty is called', () => {
-      store.commit('alert/setAl', {
-        alertShow: true,
-        alertMessage: 'Testing testing 123',
-        alertType: 'error',
-        alertLink: null,
-        linkText: ''
-      })
-      expect(store.getters['alert/getAlert']).toStrictEqual({
-        alertShow: true,
-        alertMessage: 'Testing testing 123',
-        alertType: 'error',
-        alertLink: null,
-        linkText: ''
-      })
-      store.commit('alert/setEmpty')
+  describe('Global Alert', () => {
+    it('should initially have a show key that is false and other keys should be falsey', () => {
       expect(store.getters['alert/getAlert']).toStrictEqual({
         alertShow: false,
         alertMessage: '',
@@ -68,30 +22,114 @@ describe('Alert Vuex Module', () => {
         linkText: ''
       })
     })
+
+    describe('mutations', () => {
+      it('updates the state when setAl is called', () => {
+        expect(store.getters['alert/getAlert']).toStrictEqual({
+          alertShow: false,
+          alertMessage: '',
+          alertType: null,
+          alertLink: null,
+          linkText: ''
+        })
+        store.commit('alert/setAl', {
+          alertShow: true,
+          alertMessage: 'Testing testing 123',
+          alertType: 'error',
+          alertLink: null,
+          linkText: ''
+        })
+        expect(store.getters['alert/getAlert']).toStrictEqual({
+          alertShow: true,
+          alertMessage: 'Testing testing 123',
+          alertType: 'error',
+          alertLink: null,
+          linkText: ''
+        })
+      })
+      it('empties the state when setEmpty is called', () => {
+        store.commit('alert/setAl', {
+          alertShow: true,
+          alertMessage: 'Testing testing 123',
+          alertType: 'error',
+          alertLink: null,
+          linkText: ''
+        })
+        expect(store.getters['alert/getAlert']).toStrictEqual({
+          alertShow: true,
+          alertMessage: 'Testing testing 123',
+          alertType: 'error',
+          alertLink: null,
+          linkText: ''
+        })
+        store.commit('alert/setEmpty')
+        expect(store.getters['alert/getAlert']).toStrictEqual({
+          alertShow: false,
+          alertMessage: '',
+          alertType: null,
+          alertLink: null,
+          linkText: ''
+        })
+      })
+    })
+
+    describe('actions', () => {
+      test('setAlert sets the new alert state, calls setTimeout and then empties the alert state', async () => {
+        await store.dispatch('alert/setAlert', {
+          alertShow: true,
+          alertMessage: 'Testing testing 234',
+          alertType: 'error',
+          alertLink: null,
+          linkText: ''
+        })
+        expect(store.getters['alert/getAlert']).toStrictEqual({
+          alertShow: true,
+          alertMessage: 'Testing testing 234',
+          alertType: 'error',
+          alertLink: null,
+          linkText: ''
+        })
+        expect(setTimeout).toHaveBeenCalledTimes(1)
+        expect(setTimeout).toHaveBeenLastCalledWith(
+          expect.any(Function),
+          6000,
+          'setEmpty'
+        )
+      })
+    })
   })
 
-  describe('actions', () => {
-    test('setAlert sets the new alert state, calls setTimeout and then empties the alert state', async () => {
-      await store.dispatch('alert/setAlert', {
-        alertShow: true,
-        alertMessage: 'Testing testing 234',
-        alertType: 'error',
-        alertLink: null,
-        linkText: ''
+  describe('Notifications', () => {
+    describe('getters', () => {
+      it('should return notifications', () => {
+        const notifications = ['foo', 'bar', 'batz']
+
+        const store = new Vuex.Store({
+          state: { ...alert.state, notifications: [...notifications] },
+          getters: alert.getters
+        })
+
+        expect(store.getters.notifications).toStrictEqual(notifications)
       })
-      expect(store.getters['alert/getAlert']).toStrictEqual({
-        alertShow: true,
-        alertMessage: 'Testing testing 234',
-        alertType: 'error',
-        alertLink: null,
-        linkText: ''
+
+      it('should return notificationTimeouts', () => {
+        const notificationTimeouts = { 0: 123, 1: 456 }
+
+        const store = new Vuex.Store({
+          state: {
+            ...alert.state,
+            notificationTimeouts: Object.assign({}, notificationTimeouts)
+          },
+          getters: alert.getters
+        })
+
+        expect(store.getters.notificationTimeouts).toStrictEqual(
+          notificationTimeouts
+        )
       })
-      expect(setTimeout).toHaveBeenCalledTimes(1)
-      expect(setTimeout).toHaveBeenLastCalledWith(
-        expect.any(Function),
-        6000,
-        'setEmpty'
-      )
     })
+
+    describe('mutations', () => {})
+    describe('actions', () => {})
   })
 })
