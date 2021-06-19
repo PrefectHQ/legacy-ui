cdd206af3c5e48da34a88fb5a1e2410ab1f8ce58
Natalie Smith <thatgalnatalie@gmail.com>
2021-06-18 18:07:15 -0700

    rm titleCase computed prop

--- src/pages/TeamSettings/Service-Accounts.vue
+++ src/pages/TeamSettings/Service-Accounts.vue
@@ -166,14 +166,6 @@ export default {
       this.serviceAccountNameInput = event.firstName
       this.serviceAccountID = event.membershipID
       this.dialogAddServiceAccount = true
-    },
-    titleCase(str) {
-      return str
-        .split('_')
-        .map(
-          word => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
-        )
-        .join(' ')
     }
   },
   apollo: {
