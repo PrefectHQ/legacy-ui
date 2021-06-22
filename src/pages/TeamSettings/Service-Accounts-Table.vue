75efdeee5cef1671a5b308151222a0105b5a65b5
Natalie Smith <thatgalnatalie@gmail.com>
2021-06-22 16:49:34 -0700

    check for delete permission in service account expandable

--- src/pages/TeamSettings/Service-Accounts-Table.vue
+++ src/pages/TeamSettings/Service-Accounts-Table.vue
@@ -333,7 +333,10 @@ export default {
                     : `Expires ${formatTimeRelative(key.expires)}`
                 }}</v-list-item-subtitle
               >
-              <v-tooltip bottom>
+              <v-tooltip
+                v-if="hasPermission('delete', 'service-api-key')"
+                bottom
+              >
                 <template #activator="{ on }">
                   <v-btn
                     text
