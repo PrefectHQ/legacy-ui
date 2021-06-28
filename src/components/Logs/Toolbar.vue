bcc7117ee1813116eb3fb4e9861ba055f0c9f2e9
nicholas <znicholasbrown@gmail.com>
2021-06-28 13:31:34 -0400

    Text search is working nicely

--- src/components/Logs/Toolbar.vue
+++ src/components/Logs/Toolbar.vue
@@ -23,11 +23,19 @@ export default {
       tableOptions: [
         { text: 'Team', value: 'tenant' },
         { text: 'User', value: 'user' },
-        { text: 'Flow', value: 'flow' },
-        { text: 'Flow fun', value: 'flow_run' },
-        { text: 'Task', value: 'task' },
-        { text: 'Task run', value: 'task_run' }
-      ]
+        { text: 'Flow', value: 'flow' }
+        // I don't think we'll be populating these yet for this table
+        // { text: 'Flow run', value: 'flow_run' },
+        // { text: 'Task', value: 'task' },
+        // { text: 'Task run', value: 'task_run' }
+      ],
+      textSearch: null,
+      previousSearch: null
+    }
+  },
+  computed: {
+    searchDisabled() {
+      return this.textSearch == this.previousSearch
     }
   },
   mounted() {
@@ -64,35 +72,82 @@ export default {
       'table',
       val => {
         this.filter.object_table = { _eq: val }
-
         this.$emit('input', { ...this.filter })
       },
       {
         immediate: true
       }
     )
+  },
+  methods: {
+    updateTextSearch() {
+      if (this.searchDisabled) return
+
+      // Performing this check lets us make a more efficient query
+      if (this.textSearch) {
+        this.filter.message = { _ilike: `%${this.textSearch}%` }
+      } else {
+        delete this.filter.message
+      }
+
+      this.previousSearch = this.textSearch
+
+      this.$emit('input', { ...this.filter })
+    }
   }
 }
 </script>
 
 <template>
-  <div class="d-flex align-middle justify-end">
-    <DateTimePicker
-      v-if="start"
-      v-model="start"
-      label="Start date"
-      icon="today"
-    />
-    <DateTimePicker v-if="end" v-model="end" label="End date" icon="event" />
+  <div class="d-flex align-middle justify-end pa-4">
+    <div
+      class="mr-auto d-flex align-center justify-center"
+      style="max-width: 350px;"
+    >
+      <v-text-field
+        v-model="textSearch"
+        label="Search"
+        outlined
+        hide-details
+        dense
+        append-icon="search"
+        clearable
+        @keyup.enter="updateTextSearch"
+      />
+      <v-btn
+        class="ml-2"
+        color="primary"
+        depressed
+        small
+        :disabled="searchDisabled"
+        @click="updateTextSearch"
+      >
+        Search
+      </v-btn>
+    </div>
 
-    <v-select
-      v-model="table"
-      outlined
-      label="Type"
-      :items="tableOptions"
-      dense
-      hide-details
-    />
+    <div class="mr-4" style="max-width: 200px;">
+      <v-select
+        v-model="table"
+        outlined
+        label="Type"
+        :items="tableOptions"
+        dense
+        hide-details
+      />
+    </div>
+
+    <div class="d-inline-flex align-center justify-center">
+      <div class="mr-4">
+        <DateTimePicker
+          v-if="start"
+          v-model="start"
+          label="Start date"
+          icon="today"
+        />
+      </div>
+      <DateTimePicker v-if="end" v-model="end" label="End date" icon="event" />
+    </div>
   </div>
 </template>
 
