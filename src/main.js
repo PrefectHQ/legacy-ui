674e5f43df0d05a948d59e28f079f691c15534ee
nicholas <znicholasbrown@gmail.com>
2021-08-04 17:01:10 -0400

    Move logrocket to first executed code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
+import '@/plugins/logrocket.js'
 import { login, switchTenant, commitTokens } from '@/auth/index.js'
-import { initializeLogrocket } from '@/plugins/logrocket.js'
 import { CreatePrefectUI } from '@/app.js'
 import store from '@/store'
 import jwt_decode from 'jwt-decode'
@@ -131,12 +131,6 @@ export const start = async () => {
   CreatePrefectUI()
 }
 
-try {
-  initializeLogrocket()
-} catch (e) {
-  // eslint-disable-next-line no-console
-  console.error('Failed to initialize LogRocket: ', e)
-}
 start()
 
 // ******************************************************************************************
