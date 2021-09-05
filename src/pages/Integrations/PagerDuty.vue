0ff2c474e9695f2c1c501d195a1474e2bffacdd1
kingsleyb <kingsley@prefect.io>
2021-09-04 21:31:05 -0400

    Updated to be a single step to save integration keys. I have a placeholder list in for now that needs to be removed. ToDo: 1. create a list of keys form integration_keys 2. iterate over that list for each row 3. add method to be able to delete from list to be saved 4. activate save when ready 5. fix undefined in action name to be saved 5. Add a header explaining the page 6. Add a trigger button with a test trigger

--- src/pages/Integrations/PagerDuty.vue
+++ src/pages/Integrations/PagerDuty.vue
@@ -1,7 +1,5 @@
 <script>
-import { actionTypes, jsonPlacehold } from '@/utils/automations'
 import { mapGetters } from 'vuex'
-import jsBeautify from 'js-beautify'
 
 export default {
   components: {},
@@ -20,9 +18,7 @@ export default {
     return {
       integration_keys: JSON.parse(this.pdData).integration_keys,
       account: JSON.parse(this.pdData).account,
-      jsonPlacehold: jsonPlacehold.jsonBlob,
       steps: {
-        openMessageText: { name: 'openMessageText', complete: false },
         openToConfig: { name: 'openToConfig', complete: false },
         addName: { name: 'addName', complete: false }
       },
@@ -32,11 +28,7 @@ export default {
       step: null,
       actionConfig: null,
       actionConfigArray: [],
-      messageName: 'message',
-      messageText: '',
       newSaveAs: '',
-      jsonPayload: null,
-      validJson: true,
       routingKey: '',
       severity: '',
       severityLevels: [
@@ -62,51 +54,11 @@ export default {
   computed: {
     ...mapGetters('api', ['isCloud']),
     ...mapGetters('user', ['user']),
-    messagePlaceholder() {
-      let messageText
-      switch (this.eventType) {
-        case 'CHANGES_STATE':
-          messageText =
-            'Run {flow_run_name} of flow {flow_name} entered state {state} with message {state_message}. See {flow_run_link} for more details.'
-          break
-        case 'SCHEDULED_NOT_STARTED':
-          messageText =
-            'Run {flow_run_name} ({flow_run_id}) of flow {flow_name} failed {kind} SLA {flow_sla_config_id} after {duration_seconds} seconds. See {flow_run_link} for more details.'
-          break
-        case 'STARTED_NOT_FINISHED':
-          messageText =
-            'Run {flow_run_name} ({flow_run_id}) of flow {flow_name} failed {kind} SLA {flow_sla_config_id} after {duration_seconds} seconds. See {flow_run_link} for more details.'
-          break
-        case 'AGENT':
-          messageText =
-            'Agents sharing the config {agent_config_id} have failed the minimum healthy count of {sla_min_healthy}. The following agents are unhealthy: {agent_ids}'
-          break
-        default:
-          'You have a notification from Prefect Cloud'
-      }
-      messageText =
-        'TODO I need to consider the event type and the recommended text for the action'
-      return messageText
-    },
-    disableNext() {
-      if (this.step.name === 'openToConfig') {
-        if (this.routingKey && this.severity) return false
-        if (!this.actionConfigArray.length) return true
-        else return false
-      } else {
-        if (this.jsonPayload && !this.validJson) return true
-        return false
-      }
-    },
     allowSave() {
-      const type = this.actionType.type
       let allow = this.isPagerDuty
         ? !!this.actionType && !!this.routingKey && !!this.severity
-        : !!this.actionType &&
-          !!this.actionConfigArray.length &&
-          this.actionConfigArray.filter(item => this.rules[type](item) !== true)
-            .length < 1
-      return allow && this.step.name === 'addName'
+        : !!this.actionType && !!this.actionConfigArray.length
+      return allow
     },
     saveAs: {
       get() {
@@ -121,66 +73,11 @@ export default {
     }
   },
   created() {
-    this.step = this.steps['openMessageText']
+    this.step = this.steps['openToConfig']
   },
   methods: {
-    jsonPlaceholder() {
-      this.jsonPlacehold.message = this.messagePlaceholder
-      return jsBeautify(JSON.stringify(this.jsonPlacehold))
-    },
     buttonColor(selectedStep) {
       return this.step.name === selectedStep ? 'codePink' : 'utilGrayDark'
-    },
-    format(selectedStep, otherStep) {
-      const stepComplete = this.steps[selectedStep]
-      const otherComplete = this.steps[otherStep]
-      return this.step.name === selectedStep || this.step.name === otherStep
-        ? 'font-weight-dark'
-        : stepComplete?.complete || otherComplete?.complete
-        ? 'font-weight-light'
-        : ''
-    },
-    switchStep(selectedStep) {
-      if (this.step.name === 'openMessageText')
-        this.steps['openMessageText'].complete = true
-      this.step = this.steps[selectedStep]
-    },
-    handleJsonValidation(event) {
-      this.validJson = !event
-    },
-    handleNext() {
-      if (this.step.name === 'openMessageText') {
-        this.steps['openMessageText'].complete = true
-        this.switchStep('openToConfig')
-      } else if (this.step.name === 'openToConfig') {
-        if (this.actionType.type === 'TWILIO') {
-          if (this.actionConfigArray.length)
-            this.steps['openToConfig'].complete = true
-          this.switchStep('addTwilioConfig')
-        } else {
-          if (this.actionType.type === 'EMAIL' && this.actionConfigArray.length)
-            this.steps['openToConfig'].complete = true
-          else if (this.apiToken && this.routingKey && this.severity) {
-            this.steps['openToConfig'].complete = true
-          }
-          this.switchStep('addName')
-        }
-      } else {
-        this.switchStep('addName')
-      }
-    },
-    selectActionType(type) {
-      this.actionType = type
-      this.actionConfigArray = []
-      this.steps = {
-        selectActionType: { name: 'selectActionType', complete: false },
-        openMessageText: { name: 'openMessageText', complete: false },
-        openToConfig: { name: 'openToConfig', complete: false },
-        addTwilioConfig: { name: 'addTwilioConfig', complete: false },
-        addName: { name: 'addName', complete: false }
-      }
-      this.steps.selectActionType.complete = true
-      this.switchStep('openMessageText')
     },
     handleClose() {
       this.$emit('close-action')
@@ -202,9 +99,6 @@ export default {
                   routing_key: this.routingKey,
                   severity: this.severity
                 }
-                if (this.messageText) {
-                  this.actionConfig.message = this.messageText
-                }
               }
               break
           }
@@ -212,17 +106,6 @@ export default {
       } else {
         this.errorMessage = checked
       }
-    },
-    saveMessage() {
-      this.steps['openMessageText'].complete = true
-      this.switchStep('openToConfig')
-    },
-    actionTypes() {
-      let allHooks
-      allHooks = actionTypes
-      return allHooks.filter(
-        t => (t.requiresCloud && this.isCloud) || !t.requiresCloud
-      )
     },
     createAction() {
       this.saving = true
@@ -240,6 +123,10 @@ export default {
       const name = this.newSaveAs || this.saveAs
       const input = { name: name, config }
       this.$emit('new-action', input)
+    },
+    deleteItemFromList() {
+      //TODO to delete item from a list to not include in save
+      return true
     }
   }
 }
@@ -256,78 +143,8 @@ export default {
     <v-card-text class="text-h6 font-weight-light">
       <v-row>
         <v-col cols="9" lg="10">
-          <span v-if="actionType.sendText" class="mr-1">{{
-            actionType.sendText
-          }}</span>
-          <v-btn
-            :style="{ 'text-transform': 'none', 'min-width': '0px' }"
-            :color="buttonColor('selectActionType')"
-            :class="format('selectActionType')"
-            class="px-0 pb-1 text-h6"
-            text
-            @click="switchStep('selectActionType')"
-            ><span>{{ actionType.title }}</span></v-btn
-          >
-          {{ ' ' }}
-          <span>
-            <v-btn
-              v-if="!messageText"
-              :style="{ 'text-transform': 'none', 'min-width': '0px' }"
-              class="px-0 pb-1 text-h6 d-inline-block text-truncate"
-              max-width="300px"
-              text
-              :disabled="!actionType.type"
-              :color="buttonColor('openMessageText')"
-              :class="format('openMessageText')"
-              @click="switchStep('openMessageText')"
-            >
-              <span v-if="isWebhook" class="mr-1"> JSON </span> message</v-btn
-            >
-            <v-tooltip v-else top>
-              <template #activator="{ on, attrs }">
-                <v-btn
-                  :style="{ 'text-transform': 'none', 'min-width': '0px' }"
-                  class="px-0 pb-1 text-h6 d-inline-block text-truncate"
-                  max-width="300px"
-                  text
-                  :disabled="!actionType.type"
-                  :color="buttonColor('openMessageText')"
-                  :class="format('openMessageText')"
-                  v-bind="attrs"
-                  @click="switchStep('openMessageText')"
-                  v-on="on"
-                >
-                  message</v-btn
-                ></template
-              >{{ messageText }}</v-tooltip
-            >
-          </span>
-          <span>
-            <v-btn
-              :style="{ 'text-transform': 'none', 'min-width': '0px' }"
-              class="pb-1 px-0 ml-1 text-h6"
-              text
-              :disabled="!actionType.type"
-              :color="buttonColor('openToConfig')"
-              :class="format('openToConfig')"
-              @click="switchStep('openToConfig')"
-              >{{ to }}</v-btn
-            >
-            <span v-if="isTwilio">
-              using this
-              <v-btn
-                :style="{ 'text-transform': 'none', 'min-width': '0px' }"
-                class="px-0 pb-1 text-h6"
-                text
-                :color="buttonColor('addTwilioConfig')"
-                :class="format('openToConfig')"
-                @click="switchStep('addTwilioConfig')"
-              >
-                config.</v-btn
-              ></span
-            ></span
-          ></v-col
-        >
+          <span class="mr-1">Some Text Here</span>
+        </v-col>
         <v-col cols="3" lg="2" class="text-right">
           <v-btn
             text
@@ -347,63 +164,7 @@ export default {
         >
       </v-row>
     </v-card-text>
-
-    <v-card-text v-if="step.name === 'openMessageText'" class="pt-0">
-      <div>
-        <span class="primary--text"
-          >Type your message here or leave blank to send a default message.
-        </span>
-        <v-menu
-          v-model="menu"
-          :close-on-content-click="false"
-          :open-on-hover="true"
-        >
-          <template #activator="{ on, attrs }">
-            <v-btn icon small v-bind="attrs" v-on="on">
-              <v-icon color="primary" small>info</v-icon>
-            </v-btn>
-          </template>
-
-          <v-card width="30vW">
-            <v-card-text
-              ><div class="mb-2"
-                >The default message varies depending on what type of event you
-                attach the action to. You can also send a custom message.
-
-                <div class="mt-2">
-                  Attributes you can include in a custom message include:
-                  <ul
-                    ><li>event_id</li>
-                    <li>flow_name</li
-                    ><li> flow_run_name</li>
-                    <li>flow_run_id</li>
-                    <li>agent_ids</li>
-                    <li>state</li>
-                    <li>state_message</li>
-                    <li>flow_run_link</li>
-                  </ul>
-                </div>
-                <div class="mt-2">
-                  For example:
-                  <div>{{
-                    `"Run {flow_run_name} from flow {flow_name} needs
-                your attention! See: {flow_run_link}"`
-                  }}</div>
-                </div>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-menu>
-        <v-textarea
-          v-model="messageText"
-          class="pt-0"
-          outlined
-          :placeholder="messagePlaceholder"
-          @keydown.enter="saveMessage"
-        />
-      </div>
-    </v-card-text>
-    <v-card-text v-else-if="step.name === 'openToConfig'">
+    <v-card-text>
       <div>
         <span>
           Prefect Cloud will send a PagerDuty notification. Create your
@@ -421,8 +182,8 @@ export default {
           >
           integration.
         </span>
-        <v-row class="mt-2">
-          <v-col cols="12" md="4">
+        <v-row v-for="i in [1, 2, 3, 4]" :key="i" class="mt-2">
+          <v-col cols="12" md="3">
             <v-select
               v-model="severity"
               :items="severityLevels"
@@ -438,34 +199,34 @@ export default {
               class="mb-8"
             />
           </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="saveAs"
+              label="Action Name"
+              outlined
+              class="mb-8"
+            />
+          </v-col>
+          <v-col cols="12" md="1">
+            <v-tooltip bottom>
+              <template #activator="{ on }">
+                <v-btn
+                  text
+                  fab
+                  x-small
+                  color="error"
+                  v-on="on"
+                  @click="deleteItemFromList()"
+                >
+                  <v-icon>delete</v-icon>
+                </v-btn>
+              </template>
+              Revoke token
+            </v-tooltip>
+          </v-col>
         </v-row>
       </div>
     </v-card-text>
-
-    <v-card-text v-else-if="step.name === 'addName'" class="pr-4">
-      <div class="pb-2"
-        >Give your action a name so you can find and re-use it with more
-        automations.</div
-      >
-      <v-text-field
-        v-model="saveAs"
-        outlined
-        class="pr-4"
-        label="Save As"
-      ></v-text-field>
-    </v-card-text>
-    <v-card-actions v-if="step.name !== 'addName'">
-      <v-spacer></v-spacer>
-      <v-btn
-        class="text-normal mr-1 mb-1"
-        depressed
-        :disabled="disableNext"
-        color="primary"
-        title="Next"
-        @click="handleNext"
-        ><span style="text-transform: none;"> Next</span>
-      </v-btn>
-    </v-card-actions>
   </v-card>
 </template>
 
