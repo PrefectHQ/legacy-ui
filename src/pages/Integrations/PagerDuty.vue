db56e7bbedb5753dc164c0e8140ac6e2ea930f82
Jennifer Grange <jenny@prefect.io>
2021-09-17 09:52:52 -0400

    add a nicer add button to match run flow page styling

--- src/pages/Integrations/PagerDuty.vue
+++ src/pages/Integrations/PagerDuty.vue
@@ -163,7 +163,7 @@ export default {
 </script>
 
 <template>
-  <v-card class="ma-8 pdcard" max-height="60vh">
+  <v-card class="ma-8 pa-4 pdcard" max-height="60vh">
     <v-card-text class="text-h4 font-weight-light">
       <v-row class="pt-2">
         <v-col cols="9" lg="10">
@@ -185,7 +185,7 @@ export default {
       </v-row>
     </v-card-text>
     <v-card-text>
-      <div>
+      <div class="mb-4">
         <span>
           Create an action to use with Prefect Automations and the Pager Duty
           <a
@@ -194,100 +194,92 @@ export default {
             >Events API v2</a
           >.
         </span>
-        <v-row v-if="!integrationKeys || !integrationKeys.length">
-          <div
-            class="text-center position-absolute center-absolute text-h5 utilGrayDark--text mt-10"
-            :style="{ 'z-index': 1, width: '100%' }"
-          >
-            No integration keys
-          </div>
-        </v-row>
-        <v-row
-          class="text-center"
-          v-else-if="
-            integrationKeys && integrationKeys.length === successIds.length
-          "
+      </div>
+      <div v-if="!integrationKeys || !integrationKeys.length">
+        <div
+          class="text-center position-absolute center-absolute text-h5 utilGrayDark--text mt-10"
+          :style="{ 'z-index': 1, width: '100%' }"
         >
-          <div
-            class="text-center position-absolute center-absolute text-h5 utilGrayDark--text mt-10"
-            :style="{ 'z-index': 1, width: '100%' }"
-          >
-            <v-data-table
-              fixed-header
-              :headers="headersByViewport"
-              :items="actions"
-              :items-per-page="10"
-              show-expand
-              :loading="loadingTable || !actions || !actions.length"
-              class="elevation-2 rounded-0 truncate-table"
-              :class="{ 'fixed-table': $vuetify.breakpoint.smAndUp }"
-              :footer-props="{
-                showFirstLastPage: true,
-                itemsPerPageOptions: [10, 15, 20, -1],
-                firstIcon: 'first_page',
-                lastIcon: 'last_page',
-                prevIcon: 'keyboard_arrow_left',
-                nextIcon: 'keyboard_arrow_right'
-              }"
-              no-data-text="No actions."
-            >
-              <!-- ACTION ID-->
-              <template #item.id="{ item }">
-                {{ item.id }}
-              </template>
+          No integration keys
+        </div>
+      </div>
+      <div
+        class="text-center"
+        v-else-if="
+          integrationKeys && integrationKeys.length === successIds.length
+        "
+      >
+        <v-data-table
+          fixed-header
+          :headers="headersByViewport"
+          :items="actions"
+          :items-per-page="10"
+          show-expand
+          :loading="loadingTable || !actions || !actions.length"
+          class="elevation-2 rounded-0 truncate-table"
+          :class="{ 'fixed-table': $vuetify.breakpoint.smAndUp }"
+          :footer-props="{
+            showFirstLastPage: true,
+            itemsPerPageOptions: [10, 15, 20, -1],
+            firstIcon: 'first_page',
+            lastIcon: 'last_page',
+            prevIcon: 'keyboard_arrow_left',
+            nextIcon: 'keyboard_arrow_right'
+          }"
+          no-data-text="No actions."
+        >
+          <!-- ACTION ID-->
+          <template #item.id="{ item }">
+            {{ item.id }}
+          </template>
 
-              <!-- ACTION NAME -->
-              <template #item.name="{ item }">
-                <div class="hidewidth">
-                  {{ item.name }}
-                </div>
-              </template>
-
-              <template #item.type="{ item }">
-                {{ item.action_type }}
-              </template>
-
-              <!-- ACTION CONFIG -->
-              <template v-slot:expanded-item="{ headers, item }">
-                <td
-                  v-if="Object.keys(item.action_config).length"
-                  :colspan="headers.length"
-                >
-                  <DictInput
-                    v-if="Object.keys(item.action_config).length"
-                    :disableEdit="true"
-                    disabled
-                    :dict="item.action_config"
-                    :rules="[1, 2]"
-                  />
-                </td>
-                <td v-else :colspan="headers.length">
-                  No action config
-                </td>
-              </template>
-            </v-data-table>
-
-            <div class="mt-4 text-center">
-              <v-btn
-                color="primary"
-                outlined
-                class="mr-4"
-                :to="{ name: 'actions' }"
-                >Manage Actions</v-btn
-              >
-              <v-btn
-                color="primary"
-                :to="{
-                  path: `/${tenant.slug}?automations=`
-                }"
-                >Create Automation</v-btn
-              >
+          <!-- ACTION NAME -->
+          <template #item.name="{ item }">
+            <div class="hidewidth">
+              {{ item.name }}
             </div>
-          </div>
-        </v-row>
+          </template>
+
+          <template #item.type="{ item }">
+            {{ item.action_type }}
+          </template>
+
+          <!-- ACTION CONFIG -->
+          <template v-slot:expanded-item="{ headers, item }">
+            <td
+              v-if="Object.keys(item.action_config).length"
+              :colspan="headers.length"
+            >
+              <DictInput
+                v-if="Object.keys(item.action_config).length"
+                :disableEdit="true"
+                disabled
+                :dict="item.action_config"
+                :rules="[1, 2]"
+              />
+            </td>
+            <td v-else :colspan="headers.length">
+              No action config
+            </td>
+          </template>
+        </v-data-table>
+
+        <div class="mt-6" align="center" justify="center">
+          <v-btn color="primary" outlined class="mr-4" :to="{ name: 'actions' }"
+            >Manage Actions</v-btn
+          >
+          <v-btn
+            color="primary"
+            :to="{
+              path: `/${tenant.slug}?automations=`
+            }"
+            >Create Automation</v-btn
+          >
+        </div>
+      </div>
+      <div v-else>
         <v-row
           v-for="(key, index) in integrationKeys"
-          v-else
           :key="index"
           class="mt-2"
         >
@@ -312,20 +304,6 @@ export default {
             <v-text-field v-model="key.name" label="Action Name" outlined />
           </v-col>
           <v-col cols="12" md="1">
-            <!-- <v-tooltip bottom>
-              <template #activator="{ on }">
-                <v-btn
-                  class="mt-3"
-                  x-small
-                  color="primary"
-                  v-on="on"
-                  @click="saveConfig(key)"
-                >
-                  Save
-                </v-btn>
-              </template>
-              Save action
-            </v-tooltip> -->
             <v-tooltip bottom>
               <template #activator="{ on }">
                 <v-btn
@@ -344,14 +322,21 @@ export default {
             </v-tooltip>
           </v-col>
         </v-row>
-        <v-row>
-          <v-col cols="0" md="11" />
-          <v-col cols="12" md="1">
-            <v-btn text icon color="primary" @click="addKey"
-              ><v-icon large>add</v-icon></v-btn
-            >
-          </v-col>
-        </v-row>
+
+        <div class="text-center">
+          <v-btn
+            class="mx-auto px-8 text-none"
+            depressed
+            text
+            color="primary"
+            @click="addKey"
+          >
+            Add Action
+            <v-icon right>
+              add
+            </v-icon>
+          </v-btn>
+        </div>
       </div>
     </v-card-text>
   </v-card>
