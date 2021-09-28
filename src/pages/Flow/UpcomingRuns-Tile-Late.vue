7e97ed49879a2967049b14cb83293a9e74f0bca9
Evan Sutherland <stackoverfloweth@gmail.com>
2021-09-28 13:21:38 -0500

    fixed vue warning when either run array is null

--- src/pages/Flow/UpcomingRuns-Tile-Late.vue
+++ src/pages/Flow/UpcomingRuns-Tile-Late.vue
@@ -16,7 +16,7 @@ export default {
     },
     runs: {
       type: Array,
-      required: true
+      default: () => []
     }
   }
 }
--- src/pages/Flow/UpcomingRuns-Tile-Upcoming.vue
+++ src/pages/Flow/UpcomingRuns-Tile-Upcoming.vue
@@ -15,7 +15,7 @@ export default {
     },
     runs: {
       type: Array,
-      required: true
+      default: () => []
     }
   }
 }
