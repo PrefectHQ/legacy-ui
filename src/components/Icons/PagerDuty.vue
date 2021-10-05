836a859d2c65be4716bfd04be8ee4e95ed550be6
Jennifer Grange <jenny@prefect.io>
2021-10-05 14:41:36 -0400

    update the pager duty logo for automations and cloud hooks

--- src/components/Icons/PagerDuty.vue
+++ src/components/Icons/PagerDuty.vue
@@ -1,25 +1,21 @@
 <template>
   <svg
+    version="1.1"
+    id="Layer_1"
     xmlns="http://www.w3.org/2000/svg"
-    version="1.0"
-    width="244.000000pt"
-    height="207.000000pt"
-    viewBox="0 0 244.000000 207.000000"
-    preserveAspectRatio="xMidYMid meet"
-    class="icon"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 120.6 175"
+    style="enable-background:new 0 0 120.6 175;"
+    xml:space="preserve"
   >
-    <metadata>
-      Created by potrace 1.10, written by Peter Selinger 2001-2011
-    </metadata>
-    <g
-      transform="translate(0.000000,207.000000) scale(0.050000,-0.050000)"
-      stroke="none"
-    >
+    <g>
+      <rect y="128.4" class="st0" width="25.7" height="46.6" />
       <path
-        d="M4280 3680 l0 -420 -495 0 c-801 0 -963 -57 -1119 -390 l-56 -120 0 -650 c0 -739 7 -783 154 -949 176 -201 341 -238 1000 -227 622 11 785 76 930 372 l56 114 6 1345 5 1345 -240 0 -241 0 0 -420z m0 -1541 l0 -621 -49 -49 -49 -49 -499 0 c-461 0 -502 3 -546 38 l-47 38 0 594 0 594 47 38 c44 35 86 38 595 38 l548 0 0 -621z"
-      />
-      <path
-        d="M602 3237 c-228 -37 -402 -169 -500 -377 l-52 -110 -6 -1335 -5 -1335 240 0 241 0 0 418 0 419 585 8 c674 9 707 15 883 177 199 181 202 196 202 988 l0 680 -52 110 c-97 208 -271 327 -524 360 -179 23 -866 22 -1012 -3z m1077 -530 c39 -50 41 -76 41 -621 l0 -568 -49 -49 -49 -49 -551 0 -551 0 0 589 c0 823 -58 751 612 751 l505 0 42 -53z"
+        class="st0"
+        d="M96.5,8.6C82.8,1.2,73.2,0,50.7,0H0v106.1h25.7H29h21.5c20,0,35-1.2,48.2-10c14.4-9.5,21.9-25.4,21.9-43.8
+		C120.6,32.5,111.4,16.6,96.5,8.6z M56.4,83.9H25.7V22.7l29-0.2c26.4-0.2,39.6,9,39.6,30.1C94.3,75.3,77.9,83.9,56.4,83.9z"
       />
     </g>
   </svg>
@@ -27,6 +23,12 @@
 
 <style lang="scss" scoped>
 .icon {
+  fill: currentColor;
+}
+</style>
+
+<style type="text/css">
+.st0 {
   fill: currentColor;
 }
 </style>
