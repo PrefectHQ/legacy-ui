abeee9f54302d0502e274ceebed6d0dc7da9656b
Jennifer Grange <jenny@prefect.io>
2022-02-15 06:25:44 -0500

    remove method too

--- src/components/Artifacts/Artifacts.vue
+++ src/components/Artifacts/Artifacts.vue
@@ -38,9 +38,6 @@ export default {
     },
     increment() {
       this.artifact++
-    },
-    onIntersect([entry]) {
-      this.$apollo.queries.artifacts.skip = !entry.isIntersecting
     }
   },
   apollo: {
