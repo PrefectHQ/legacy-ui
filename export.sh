0b3b20c78ca9d4eb8a57c6ecf2f3b3b7e5a2eeea
nicholas <znicholasbrown@gmail.com>
2022-03-15 17:00:48 -0400

    Remove unecessary echos

--- export.sh
+++ export.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-echo $VUE_APP_RELEASE_TIMESTAMP
-
 touch .env
 echo "" >> .env
 
 cat ".salesforce_env_${VUE_APP_ENVIRONMENT}" | while read line; do
-    echo $line
     echo $line >> .env
 done
