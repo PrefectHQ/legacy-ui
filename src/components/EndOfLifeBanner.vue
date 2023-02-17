8e4699da8025e3c66f91e067cbf3a1e1a0cb067d
nicholas <znicholasbrown@gmail.com>
2023-02-16 19:43:39 -0500

    Add EndOfLifeBanner component

---
+++ src/components/EndOfLifeBanner.vue
@@ -0,0 +1,46 @@
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  components: {},
+  props: {
+    pageScroll: {
+      type: Boolean,
+      required: false,
+      default: () => false
+    }
+  },
+  data() {
+    return {
+      dismissed: false
+    }
+  },
+  computed: {
+    ...mapGetters('api', ['isCloud'])
+  }
+}
+</script>
+
+<template>
+  <v-banner :value="!dismissed">
+    <template #default>
+      <div class="d-flex align-center">
+        <v-avatar slot="icon" color="primary" size="40">
+          <v-icon color="white"> warning </v-icon>
+        </v-avatar>
+        <div class="ml-4"> Prefect Cloud 1 is dying :( </div>
+        <v-btn
+          class="ml-auto"
+          width="40"
+          height="40"
+          icon
+          text
+          dark
+          color="error"
+          @click="dismissed = !dismissed"
+        >
+          <v-icon> close </v-icon>
+        </v-btn>
+      </div>
+    </template>
+  </v-banner>
+</template>
