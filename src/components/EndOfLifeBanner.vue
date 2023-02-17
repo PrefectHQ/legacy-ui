541116fa6c18333dff686867927ee45ebd237e1a
nicholas <znicholasbrown@gmail.com>
2023-02-16 21:39:47 -0500

    Add links

--- src/components/EndOfLifeBanner.vue
+++ src/components/EndOfLifeBanner.vue
@@ -1,7 +1,9 @@
 <script>
 import { mapGetters } from 'vuex'
+import ExternalLink from '@/components/ExternalLink'
 
 export default {
+  components: { ExternalLink },
   computed: {
     ...mapGetters('api', ['isCloud']),
     ...mapGetters('license', ['planType']),
@@ -21,7 +23,15 @@ export default {
         </v-avatar>
         <div v-if="free" class="ml-4">
           This legacy Prefect Cloud account will be frozen on May 1st, 2023.
-          Click here to learn more or start your migration to Prefect Cloud 2.
+          <ExternalLink
+            href="https://www.prefect.io/guide/blog/freezing-legacy-prefect-cloud-1-accounts-on-starter-and-standard-plans/"
+            >Click here</ExternalLink
+          >
+          to learn more or to start your migration to
+          <ExternalLink
+            href="https://www.prefect.io/guide/blog/introducing-prefect-cloud-2-0/"
+            >Prefect Cloud 2</ExternalLink
+          >.
         </div>
         <div v-else class="ml-4">
           This legacy Prefect Cloud account will be frozen on July 15th, 2023.
